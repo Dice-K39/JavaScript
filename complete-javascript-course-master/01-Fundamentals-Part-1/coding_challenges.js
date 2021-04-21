b67d31a1e3d0c24e8a0b1d6ddca91811b2b615e6
Dice-K39 <daisukehashimoto39@gmail.com>
2021-04-21 13:21:58 -0400

    Coding challenge on decisions and printing a message out to the console.

--- complete-javascript-course-master/01-Fundamentals-Part-1/coding_challenges.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/coding_challenges.js
@@ -4,25 +4,26 @@ const bmiCalculator = (mass, height) =>
     return mass / (height ** 2)
 }
 
-const markMass1 = 78;
-const markHeight1 = 1.69;
-const johnMass1 = 92;
-const johnHeight1 = 1.95;
-const markMass2 = 95;
-const markHeight2 = 1.88;
-const johnMass2 = 85;
-const johnHeight2 = 1.76;
+const markMass = 78;
+const markHeight = 1.69;
+const johnMass = 92;
+const johnHeight = 1.95;
 
-const markBMI1 = bmiCalculator(markMass1, markHeight1);
-const johnBMI1 = bmiCalculator(johnMass1, johnHeight1);
-const markBMI2 = bmiCalculator(markMass2, markHeight2);
-const johnBMI2 = bmiCalculator(johnMass2, johnHeight2);
+const markBMI = bmiCalculator(markMass, markHeight);
+const johnBMI = bmiCalculator(johnMass, johnHeight);
 
-console.log(markBMI1, johnBMI1);
-console.log(markBMI2, johnBMI2);
+// console.log(markBMI, johnBMI);
 
-const markHigherBMI1 = markBMI1 > johnBMI1;
-const markHigherBMI2 = markBMI2 > johnBMI2;
+const markHigherBMI = markBMI > johnBMI;
 
-console.log(markHigherBMI1);
-console.log(markHigherBMI2);
+// console.log(markHigherBMI);
+
+// Challenge 2
+if (markHigherBMI)
+{
+    console.log(`Mark's BMI (${markBMI}) is higher than John's(${johnBMI})!`)
+}
+else
+{
+    console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`)
+}
