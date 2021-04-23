0ab2e62cb42ce7a88e9a55bb45358e72db9cba3b
Dice-K39 <daisukehashimoto39@gmail.com>
2021-04-23 00:28:21 -0400

    Recap and assignment on Equality Operators: == vs. ===.

--- complete-javascript-course-master/01-Fundamentals-Part-1/assignment.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/assignment.js
@@ -51,3 +51,19 @@ const ex4 = "123" < 57;
 const ex5 = 5 + 6 + '4' + 9 - 4 - 2;
 
 console.log(ex1, ex2, ex3, ex4, ex5);
+
+// Equality Operators: == vs. ===
+const numNeighbors = Number(prompt("How many neighbor countries does your country have?"));
+
+if (numNeighbors === 1)
+{
+    console.log(`Only 1 border!`);
+}
+else if (numNeighbors > 1)
+{
+    console.log(`More than 1 border!`);
+}
+else
+{
+    console.log(`No borders!`);
+}
