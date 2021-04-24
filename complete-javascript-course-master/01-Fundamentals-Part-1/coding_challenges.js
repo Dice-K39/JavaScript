e8d76d8db596daefc0f4b5092a559fe56a2b7b30
Dice-K39 <daisukehashimoto39@gmail.com>
2021-04-23 23:42:53 -0400

    Used conditional statements with logical operators in coding challenge 3.

--- complete-javascript-course-master/01-Fundamentals-Part-1/coding_challenges.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/coding_challenges.js
@@ -1,29 +1,50 @@
 // Challenge 1
-const bmiCalculator = (mass, height) =>
-{
-    return mass / (height ** 2)
-}
+// const bmiCalculator = (mass, height) =>
+// {
+//     return mass / (height ** 2)
+// }
 
-const markMass = 78;
-const markHeight = 1.69;
-const johnMass = 92;
-const johnHeight = 1.95;
+// const markMass = 78;
+// const markHeight = 1.69;
+// const johnMass = 92;
+// const johnHeight = 1.95;
 
-const markBMI = bmiCalculator(markMass, markHeight);
-const johnBMI = bmiCalculator(johnMass, johnHeight);
+// const markBMI = bmiCalculator(markMass, markHeight);
+// const johnBMI = bmiCalculator(johnMass, johnHeight);
 
-// console.log(markBMI, johnBMI);
+// // console.log(markBMI, johnBMI);
 
-const markHigherBMI = markBMI > johnBMI;
+// const markHigherBMI = markBMI > johnBMI;
 
 // console.log(markHigherBMI);
 
 // Challenge 2
-if (markHigherBMI)
+// if (markHigherBMI)
+// {
+//     console.log(`Mark's BMI (${markBMI}) is higher than John's(${johnBMI})!`)
+// }
+// else
+// {
+//     console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`)
+// }
+
+// Challenge 3
+const dolphins = (97 + 112 + 101) / 3;
+const koalas = (109 + 95 + 106) / 3;
+
+if (dolphins > koalas && dolphins >= 100)
 {
-    console.log(`Mark's BMI (${markBMI}) is higher than John's(${johnBMI})!`)
+    console.log("Dolphins wins a trophy!");
+}
+else if (koalas > dolphins && koalas >= 100)
+{
+    console.log("Koalas wins a trophy!");
+}
+else if (dolphins === koalas && dolphins >= 100 && koalas >= 100)
+{
+    console.log("The competition is a draw.");
 }
 else
 {
-    console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`)
+    console.log("No team wins the trophy.");
 }
