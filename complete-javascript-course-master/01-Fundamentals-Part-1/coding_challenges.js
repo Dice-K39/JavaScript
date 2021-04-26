16f2a1cfe926424e11c487e289f8934434d96c07
Dice-K39 <daisukehashimoto39@gmail.com>
2021-04-26 10:07:06 -0400

    Commenting out all assignment and challenge codes for next part.

--- complete-javascript-course-master/01-Fundamentals-Part-1/coding_challenges.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/coding_challenges.js
@@ -50,7 +50,7 @@
 // }
 
 // Challenge 4
-const bill = 275;//40 430
-const tip = (bill >= 50 && bill < 300) ? bill * 0.15 : bill * 0.2;
+// const bill = 275;//40 430
+// const tip = (bill >= 50 && bill < 300) ? bill * 0.15 : bill * 0.2;
 
-console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+// console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
