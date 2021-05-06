277afe9d730b4e62075c4e2c9abfb764d73cd2ac
Dice-K39 <daisukehashimoto39@gmail.com>
2021-05-06 03:46:29 -0400

    Recap on function declarations and expressions.

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/assignment.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/assignment.js
@@ -1,3 +1,4 @@
+// Function
 const describeCountry = (country, population, capitalCity) =>
 {
     return `${country} has ${population} million people and its capital city is ${capitalCity}`;
@@ -6,3 +7,26 @@ const describeCountry = (country, population, capitalCity) =>
 console.log(describeCountry("USA", 368, "Washington D.C."));
 console.log(describeCountry("Japan", 200, "Tokyo"));
 console.log(describeCountry("France", 300, "Paris"));
+
+// Function Declarations vs. Expressions
+const percentageOfWorld1 = (population) =>
+{
+    return (population / 7900) * 100;
+}
+
+const population1 = percentageOfWorld1(300);
+const population2 = percentageOfWorld1(100);
+const population3 = percentageOfWorld1(500);
+
+console.log(population1, population2, population3);
+
+const percentageOfWorld2 = (population) =>
+{
+    return (population / 7900) * 100;
+}
+
+const population4 = percentageOfWorld2(300);
+const population5 = percentageOfWorld2(100);
+const population6 = percentageOfWorld2(500);
+
+console.log(population4, population5, population6);
