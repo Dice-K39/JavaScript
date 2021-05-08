597fee0e3c5c5010f5af8c01d2d73b616e18b9ea
Dice-K39 <daisukehashimoto39@gmail.com>
2021-05-08 03:02:55 -0400

    Calling functions inside a function.

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/assignment.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/assignment.js
@@ -31,3 +31,15 @@ const population5 = percentageOfWorld2(100);
 const population6 = percentageOfWorld2(500);
 
 console.log(population4, population5, population6);
+
+// Functions Calling Other Functions
+const describePopulation = (country, population) =>
+{
+    const percentage = percentageOfWorld1(population);
+
+    return `${country} has ${population} million people, which is about ${percentage}% of the world.`;
+}
+
+console.log(describePopulation("USA", 500));
+console.log(describePopulation("Japan", 300));
+console.log(describePopulation("China", 700));
