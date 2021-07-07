c3c2902ce890f6029a978831a7f9576402346c23
Dice-K39 <daisukehashimoto39@gmail.com>
2021-07-06 21:18:59 -0400

    Basic setup of fields complete.

--- Angular - The Complete Guide (2021 Edition)/course-code/prj-start/src/app/shopping-list/shopping-list.component.ts
+++ Angular - The Complete Guide (2021 Edition)/course-code/prj-start/src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from "../shared/ingredient.model";
+@Component(
+	{
+		selector: 'app-shopping-list',
+		templateUrl: './shopping-list.component.html',
+		styleUrls: ['./shopping-list.component.css']
+	}
+)
+export class ShoppingListComponent implements OnInit
+{
+	ingredients: Ingredient[] = [
+		new Ingredient("apples", 5),
+		new Ingredient("Tomatoes", 10)
+	];
 
-@Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
-})
-export class ShoppingListComponent implements OnInit {
+	constructor()
+	{
 
-  constructor() { }
+	}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void
+	{
+	  
+	}
 
 }
