34848ab05c9264b6144ef784b619e0f92ae9a0fb
Dice-K39 <daisukehashimoto39@gmail.com>
2021-09-03 13:26:05 -0400

    Completed Coding Challenge 3.

--- The Complete JavaScript Course 2021 - From Zero to Expert/course-code/complete-javascript-course-master/02-Fundamentals-Part-2/coding_challenges.js
+++ The Complete JavaScript Course 2021 - From Zero to Expert/course-code/complete-javascript-course-master/02-Fundamentals-Part-2/coding_challenges.js
@@ -22,3 +22,62 @@ const checkWinner = (avgDolphins, avgKoalas) =>
 
 checkWinner(calcAverage(44, 23, 71), calcAverage(65, 54, 49));
 checkWinner(calcAverage(85, 54, 41), calcAverage(23, 34, 27));
+
+// Coding Challenge 2
+const calcTip = (bill) =>
+{
+    return (bill >= 50 && bill < 300) ? bill * 0.15 : bill * 0.2;
+}
+
+const bills = [125, 555, 44];
+let tips = [];
+let total = [];
+
+bills.forEach((bill) =>
+{
+    tips.push(calcTip(bill));
+
+    total.push(bill + calcTip(bill));
+})
+
+console.log("Tips: " + tips);
+console.log("Totals: " + total)
+
+// Coding Challenge 3
+let mark =
+{
+    fullName: "Mark Miller",
+    mass: 78,
+    height: 1.69,
+    calcBMI: function()
+    {
+        this.bmi = this.mass / this.height ** 2;
+
+        return this.bmi; 
+    }
+}
+
+let john =
+{
+    fullName: "John Smith",
+    mass: 92,
+    height: 1.95,
+    calcBMI: function()
+    {
+        this.bmi = this.mass / this.height ** 2;
+
+        return this.bmi;
+    }
+}
+
+mark.calcBMI();
+john.calcBMI();
+
+if (mark.bmi > john.bmi)
+{
+    console.log(`Mark's BMI (${mark.bmi}) is higher than John's (${john.bmi})`);
+}
+else
+{
+    console.log(`John's BMI (${john.bmi}) is higher than Mark's (${mark.bmi})`);
+}
