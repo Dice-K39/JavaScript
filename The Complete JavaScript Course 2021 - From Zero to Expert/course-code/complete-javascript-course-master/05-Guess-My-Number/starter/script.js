138ca41724a1618bce1e2322258a7dcbb8c46ccb
Dice-K39 <daisukehashimoto39@gmail.com>
2021-09-09 14:53:10 -0400

    Created high low game logic. Made the code so that it works rather than follow DRY.

--- The Complete JavaScript Course 2021 - From Zero to Expert/course-code/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ The Complete JavaScript Course 2021 - From Zero to Expert/course-code/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -8,6 +8,9 @@
 
 // document.querySelector(".guess").value = 23;
 // console.log(document.querySelector(".guess").value);
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = secretNumber;
+let score = 20;
 
 document.querySelector(".check").addEventListener("click", () =>
 {
@@ -16,5 +19,41 @@ document.querySelector(".check").addEventListener("click", () =>
     if (!guess)
     {
         document.querySelector(".message").textContent = "No Number!";
+    }
+    else if (guess === secretNumber)
+    {
+        document.querySelector(".message").textContent = "Correct Number!";
+    }
+    else if (guess > secretNumber)
+    {
+        if (score > 1)
+        {
+            document.querySelector(".message").textContent = "Too high!";
+    
+            score--;
+    
+            document.querySelector(".score").textContent = score;
+        }
+        else
+        {
+            document.querySelector(".message").textContent = "You lost the game!";
+            document.querySelector(".score").textContent = 0;
+        }
+    }
+    else if (guess < secretNumber)
+    {
+        if (score > 1)
+        {
+            document.querySelector(".message").textContent = "Too low!";
+    
+            score--;
+    
+            document.querySelector(".score").textContent = score;
+        }
+        else
+        {
+            document.querySelector(".message").textContent = "You lost the game!";
+            document.querySelector(".score").textContent = 0;
+        }
     }
 });
