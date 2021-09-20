2a8625dbddfb636daa2bf7f57a2c5354dbaf760f
Dice-K39 <daisukehashimoto39@gmail.com>
2021-09-20 15:20:34 -0400

    Added functionality to New Game button that resets everything in the game.

--- The Complete JavaScript Course 2021 - From Zero to Expert/course-code/complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ The Complete JavaScript Course 2021 - From Zero to Expert/course-code/complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -12,15 +12,30 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
-// Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
+    let scores;
+    let currentScore;
+    let activePlayer;
+    let playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+// Starting conditions
+const init = () =>
+{
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+    
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    diceEl.classList.add("hidden");
+    player0El.classList.remove("player--winner");
+    player1El.classList.remove("player--winner");
+    player0El.classList.add("player--active");
+    player1El.classList.remove("player--active");
+};
 
 const switchPlayer = () =>
 {
@@ -29,7 +44,9 @@ const switchPlayer = () =>
     activePlayer = activePlayer === 0 ? 1 : 0;
     player0El.classList.toggle("player--active");
     player1El.classList.toggle("player--active");
-}
+};
+
+init();
 
 // Rolling dice functionality
 btnRoll.addEventListener("click", () =>
@@ -83,3 +100,5 @@ btnHold.addEventListener("click", () =>
         }
     }
 });
+
+btnNew.addEventListener("click", init);
