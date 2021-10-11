9006fa1a774829872f67efae177c2975d1155239
Dice-K39 <daisukehashimoto39@gmail.com>
2021-10-11 10:53:49 -0400

    Reviewed functions with callback functions.

--- The Complete JavaScript Course 2021 - From Zero to Expert/course-code/complete-javascript-course-master/10-Functions/starter/script.js
+++ The Complete JavaScript Course 2021 - From Zero to Expert/course-code/complete-javascript-course-master/10-Functions/starter/script.js
@@ -29,6 +29,7 @@ createBooking("LH234", undefined, 1000);
 /////////////////////////////////////////////////////////////
 */
 
+/*
 /////////////////////////////////////////////////////////////
 // How Passing Arguments Works: Value vs Reference
 const flight = "LH234";
@@ -68,4 +69,39 @@ const newPassport = function(person)
 
 newPassport(dicek);
 checkIn(flight, dicek);
+/////////////////////////////////////////////////////////////
+*/
+
+/////////////////////////////////////////////////////////////
+// Functions Accepting Callback Functions
+const oneWord = function(str)
+{
+    return str.replace(/ /g, '').toLowerCase();
+}
+
+const upperFirstWord = function(str)
+{
+    const [first, ...others] = str.split(' ');
+    return [first.toUpperCase(), ...others].join(' ');
+}
+
+// Higher-order function
+const transformer = function(str, fn)
+{
+    console.log(`Original string: ${str}`)
+    console.log(`Transformed string: ${fn(str)}`);
+
+    console.log(`Transformed by: ${fn.name}`);
+}
+
+transformer("JavaScript is the best!", upperFirstWord);
+transformer("JavaScript is the best!", oneWord);
+
+const high5 = function()
+{
+    console.log('👋')
+}
+document.body.addEventListener("click", high5);
+
+["Dice-K", "Martha", "Adam"].forEach(high5);
 /////////////////////////////////////////////////////////////
