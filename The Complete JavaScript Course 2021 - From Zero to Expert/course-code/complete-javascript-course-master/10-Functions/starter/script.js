7b2f66f46daffea2c68619c0eb02233a96607ce2
Dice-K39 <daisukehashimoto39@gmail.com>
2021-10-11 10:27:05 -0400

    Reviewed how passing arguments works: by value or by reference.

--- The Complete JavaScript Course 2021 - From Zero to Expert/course-code/complete-javascript-course-master/10-Functions/starter/script.js
+++ The Complete JavaScript Course 2021 - From Zero to Expert/course-code/complete-javascript-course-master/10-Functions/starter/script.js
@@ -1,5 +1,8 @@
 'use strict';
 
+/*
+/////////////////////////////////////////////////////////////
+// Default Parameters
 const bookings = [];
 
 const createBooking = function(flightNum, numPassengers = 1, price = 199 * numPassengers)
@@ -23,3 +26,46 @@ createBooking("LH123", 2, 800);
 createBooking("LH123", 5);
 
 createBooking("LH234", undefined, 1000);
+/////////////////////////////////////////////////////////////
+*/
+
+/////////////////////////////////////////////////////////////
+// How Passing Arguments Works: Value vs Reference
+const flight = "LH234";
+const dicek =
+{
+    name: "Dice-K Hash",
+    passport: 1234567890
+}
+
+const checkIn = function(flightNum, passenger)
+{
+    flightNum = "LH999";
+    passenger.name = "Mr. " + passenger.name;
+
+    if (passenger.passport === 1234567890)
+    {
+        alert("Checked in");
+    }
+    else
+    {
+        alert("Wrong passport");
+    }
+}
+
+// checkIn(flight, dicek);
+// console.log(flight);
+// console.log(dicek);
+
+// Is the same as doing...
+// const flightNum = flight;
+// const passenger = dicek;
+
+const newPassport = function(person)
+{
+    person.passport = Math.trunc(Math.random() * 1000000000);
+}
+
+newPassport(dicek);
+checkIn(flight, dicek);
+/////////////////////////////////////////////////////////////
