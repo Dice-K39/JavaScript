2e98409901426a7ad581f941da1981c843ef38f4
Dice-K39 <daisukehashimoto39@gmail.com>
2021-10-26 17:30:50 -0400

    Reviewed map method.

--- The Complete JavaScript Course 2021 - From Zero to Expert/course-code/complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ The Complete JavaScript Course 2021 - From Zero to Expert/course-code/complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -190,3 +190,24 @@ currenciesUnique.forEach(function(value, _, map)
 });
 */
 /////////////////////////////////////////////////
+
+/////////////////////////////////////////////////
+// The map Method
+
+const eurToUSD = 1.1;
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const movementsUSD = movements.map((mov) => mov * eurToUSD);
+console.log(movements);
+console.log(movementsUSD);
+
+const movementsUSDFor = [];
+
+for(const mov of movements)
+{
+  movementsUSDFor.push(mov * eurToUSD);
+}
+
+const movementsDescriptions = movements.map((mov, i) => `Movement ${i + 1}: You ${mov > 0 ? "deposited" : "withdrew"} ${Math.abs(mov)}`);
+console.log(movementsDescriptions);
+/////////////////////////////////////////////////
