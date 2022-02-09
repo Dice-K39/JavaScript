3563012f4a36f24fd3ac0792ed465a25d4a67121
Dice-K39 <daisukehashimoto39@gmail.com>
2022-02-09 10:32:12 -0500

    Completed assignment 1.

---
+++ Angular - The Complete Guide (2022 Edition)/basics-assignment-1-start/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.htm',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+}
---
+++ Angular - The Complete Guide (2022 Edition)/basics-assignment-1-start/src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component(
+    {
+        selector: "app-success-alert",
+        templateUrl: "./success-alert.component.htm",
+        styleUrls: ["./success-alert.component.css"]
+    }
+)
+
+export class SuccessAlertComponent
+{
+    constructor()
+    {
+
+    }
+
+    ngOnInit(): void
+    {
+
+    }
+}
