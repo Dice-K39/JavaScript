bc8afba241adbb3546626f092ee790d991b77305
Dice-K39 <daisukehashimoto39@gmail.com>
2022-02-11 15:59:42 -0500

    Completed assignment 3.

---
+++ Angular - The Complete Guide (2022 Edition)/basics-assignment-3-start/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  details = false;
+  // count = 0;
+  clicks = [];
+
+  displayDetails()
+  {
+    this.details = !this.details;
+    // this.clicks.push(++this.count);
+    this.clicks.push(this.clicks.length + 1);
+  }
+
+  // getBackgroundColor()
+  // {
+  //   return this.count >= 5 ? "blue" : "white";
+  // }
+}
