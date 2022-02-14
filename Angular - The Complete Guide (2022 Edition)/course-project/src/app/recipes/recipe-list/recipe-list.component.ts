918c64ee62a9148a11a4d52b6f904a624b961230
Dice-K39 <daisukehashimoto39@gmail.com>
2022-02-14 11:24:44 -0500

    Adding content to the Recipes component and looping through the recipes array to display each recipe.

--- Angular - The Complete Guide (2022 Edition)/course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular - The Complete Guide (2022 Edition)/course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,11 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit
 {
-    recipes = [];
+    recipes: Recipe[] = [
+        new Recipe(
+            "Test",
+            "Test if it works.",
+            "https://img.kidspot.com.au/50sixjAj/w643-h428-cfill-q90/kk/2020/11/easy-cinnamon-breakfast-scrolls-recipe-604936-2.jpg"),
+        new Recipe(
+            "Another Test",
+            "Another test if the loop works.",
+            "https://www.uaex.uada.edu/life-skills-wellness/food-nutrition/eating-well/EFNEP/images/Recipes-Banner.jpg")
+    ];
     
     constructor()
     {
-        
+
     }
 
     ngOnInit(): void
