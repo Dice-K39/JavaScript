6e1357994e8c2a0aa15372fd9514c5683579c3e1
Dice-K39 <daisukehashimoto39@gmail.com>
2022-03-02 14:37:58 -0500

    Understanding lifecycle hooks.

--- Angular - The Complete Guide (2022 Edition)/cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ Angular - The Complete Guide (2022 Edition)/cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,18 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import
+{
+    Component,
+    Input,
+    OnInit,
+    ViewEncapsulation,
+    OnChanges,
+    SimpleChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+} from '@angular/core';
 
 @Component(
     {
@@ -9,18 +23,45 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
     }
 )
     
-export class ServerElementComponent implements OnInit
-{
+export class ServerElementComponent
+    implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
     @Input("srvElement") element: { type: string, name: string, content: string };
+    @Input() name: string;
     
-    constructor()
-    { 
-
-    }
-    
-    ngOnInit(): void
-    {
-
+    constructor() {
+        console.log("constructor called");
     }
 
+    ngOnChanges(changes: SimpleChanges) {
+        console.log("ngOnChanges called");
+        console.log(changes);
+    }
+    
+    ngOnInit(): void {
+        console.log("ngOnInit called");
+    }
+
+    ngDoCheck() {
+        console.log("ngDoCheck called");
+    }
+
+    ngAfterContentInit() {
+        console.log("ngAfterContent called");
+    }
+
+    ngAfterContentChecked() {
+        console.log("ngAfterContentChecked called");
+    }
+
+    ngAfterViewInit() {
+        console.log("ngAfterViewInit called");
+    }
+
+    ngAfterViewChecked() {
+        console.log("ngAfterViewChecked called");
+    }
+    
+    ngOnDestroy() {
+        console.log("ngOnDestroy called");
+    }
 }
