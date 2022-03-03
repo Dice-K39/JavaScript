ec9a9294e10052c781e066b425941e2f9c113f6b
Dice-K39 <daisukehashimoto39@gmail.com>
2022-03-03 13:23:44 -0500

    Used scope functions.

--- AngularJS v1.8.2/first_app/src/app.js
+++ AngularJS v1.8.2/first_app/src/app.js
@@ -4,4 +4,9 @@ angular.module("myApp", []);
 angular.module("myApp").controller("MainController", ["$scope", function($scope)
 {
     console.log("AngularJS v1.8.2 is running successfully.");
+    $scope.message = "Hello to your new world, ";
+    $scope.sayHello = function(name)
+    {
+        return $scope.message + ' ' + name + '.';
+    }
 }]);
