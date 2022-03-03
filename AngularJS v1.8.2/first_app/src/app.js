b6e84b43c8c24c0e81c6a482779dbca4ec624d3d
Dice-K39 <daisukehashimoto39@gmail.com>
2022-03-03 13:57:55 -0500

    Learned about executing scope methods.

--- AngularJS v1.8.2/first_app/src/app.js
+++ AngularJS v1.8.2/first_app/src/app.js
@@ -1,12 +1,27 @@
 "use strict";
 
 angular.module("myApp", []);
-angular.module("myApp").controller("MainController", ["$scope", function($scope)
-{
+angular.module("myApp").controller("MainController", ["$scope", "$interval", function($scope, $interval)
+{ 
     console.log("AngularJS v1.8.2 is running successfully.");
     $scope.message = "Hello to your new world, ";
     $scope.sayHello = function(name)
     {
         return $scope.message + ' ' + name + '.';
     }
+    var items = ["bananas", "apples", "pears", "cherries", "peaches"];
+
+    $scope.itemIndex = null;
+    $scope.currentItem = '';
+
+    $scope.getItem = function()
+    {
+        $scope.currentItem = items[$scope.itemIndex];
+    }
+
+    $interval(function()
+    {
+        $scope.itemIndex = Math.round(Math.random() * (items.length - 1));
+        $scope.getItem();
+    }, 2000);
 }]);
