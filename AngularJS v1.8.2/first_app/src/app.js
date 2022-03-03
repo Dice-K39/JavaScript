b77a555c12156b9c4c1dcb941bd4695e3ba88dde
Dice-K39 <daisukehashimoto39@gmail.com>
2022-03-03 13:17:32 -0500

    Starting off AngularJS with first app.

---
+++ AngularJS v1.8.2/first_app/src/app.js
@@ -0,0 +1,7 @@
+"use strict";
+
+angular.module("myApp", []);
+angular.module("myApp").controller("MainController", ["$scope", function($scope)
+{
+    console.log("AngularJS v1.8.2 is running successfully.");
+}]);
