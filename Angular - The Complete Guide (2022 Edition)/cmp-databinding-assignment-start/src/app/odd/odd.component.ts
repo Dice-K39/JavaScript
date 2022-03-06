f5a25e8030a9b415c819b90a70372e5e30d91251
Dice-K39 <daisukehashimoto39@gmail.com>
2022-03-06 15:27:16 -0500

    Completed assignment.

--- Angular - The Complete Guide (2022 Edition)/cmp-databinding-assignment-start/src/app/odd/odd.component.ts
+++ Angular - The Complete Guide (2022 Edition)/cmp-databinding-assignment-start/src/app/odd/odd.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-odd',
   templateUrl: './odd.component.html',
   styleUrls: ['./odd.component.css']
 })
-export class OddComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class OddComponent implements OnInit
+{
+    @Input() number: number;
+    
+    constructor() { }
+    
+    ngOnInit(): void
+    {
+        
+    }
 
 }
