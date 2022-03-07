d38a8807c617fd4d48f8b64d09dd138e4044f24e
Dice-K39 <daisukehashimoto39@gmail.com>
2022-03-07 12:56:17 -0500

    Using controllers and variable scope.

--- AngularJS v1.8.2/first_app/src/app.js
+++ AngularJS v1.8.2/first_app/src/app.js
@@ -59,3 +59,11 @@ angular.module("myApp").controller("ChildController", ["$scope", function($scope
 {
 
 }]);
+
+angular.module("myApp").controller("IndieController", ["$scope", function($scope)
+{
+    $scope.model =
+    {
+        name: "Michael Johnson"
+    }
+}]);
