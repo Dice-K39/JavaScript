a3b9a72ea427407b0252d22c331b4ca8de6e203f
Daisuke Hashimoto <daisukehashimoto39@gmail.com>
2022-03-13 13:39:38 -0400

    Delete The Complete JavaScript Course 2021 - From Zero to Expert directory

--- The Complete JavaScript Course 2021 - From Zero to Expert/course-code/complete-javascript-course-master/10-Functions/coding_challenge_2.js
+++
@@ -1,9 +0,0 @@
-(function()
-{
-    const header = document.querySelector("h1");
-    header.style.color = "red";
-    document.querySelector("body").addEventListener("click", () =>
-    {
-        header.style.color = "blue";
-    })
-})();
