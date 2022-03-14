c1cb5ab30f32fbd7af1e47a86174c1f467e5df8a
Dice-K39 <daisukehashimoto39@gmail.com>
2022-03-14 11:09:28 -0400

    Review of basic operators.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -63,4 +63,36 @@ job = "programmer";
 lastName = "Hash";
 console.log(lastName);
 /////////////////////////////////////////////////////////////////
+// Basic Operators
+
+// Math operators
+const now = 2037
+const ageDice = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageDice, ageSarah);
+
+console.log(ageDice * 2, ageDice / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3, 2 * 2 * 2
+
+const firstName = "Dice";
+const lastName = "Hash";
+console.log(firstName + ' ' + lastName);
+
+// Assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--;
+x--;
+console.log(x);
+
+// Comparison operators
+console.log(ageDice > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+/////////////////////////////////////////////////////////////////
 */
