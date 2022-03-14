fd42a4999ed65abb9c759cef112b16cd45345787
Dice-K39 <daisukehashimoto39@gmail.com>
2022-03-14 09:52:07 -0400

    Recap of values and variables.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,9 +1,25 @@
 let js = "amazing";
-
-if (js === "amazing")
-{
-    alert("JavaScript is FUN!");
-}
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log("Dice");
+console.log(23);
+
+let firstName = "Ash";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let dice_hash = "DH";
+let $function = 27;
+
+let person = "dice";
+let PI = 3.1415;
+
+let myFirstJob = "Sales Rep";
+let myCurrentJob = "Programmer";
+
+let job1 = "sales rep";
+let job2 = "programmer";
+
+console.log(myFirstJob);
