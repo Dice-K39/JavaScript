9ef9eaa1da941c061164aa888a04a25cf605d730
Dice-K39 <daisukehashimoto39@gmail.com>
2022-03-14 10:54:31 -0400

    Completed assignment for let, const, and var.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -1,12 +1,17 @@
 // Values and Variables
-let country = "USA";
-let continent = "North America";
+const country = "USA";
+const continent = "North America";
 let population = 332000000;
 
 console.log(country, continent, population);
 /////////////////////////////////////////////////////////////////
 // Data Types
-let isIsland = false;
+const isIsland = false;
 let language;
 
 console.log(isIsland, population, country, language);
+/////////////////////////////////////////////////////////////////
+// let, const, and var
+language = "English";
+// isIsland = true;
+/////////////////////////////////////////////////////////////////
