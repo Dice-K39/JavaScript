5be30bc7e5c92917cf80e122256f04d118ad554d
Dice-K39 <daisukehashimoto39@gmail.com>
2022-03-14 10:33:39 -0400

    Fixed comments and completed data types assignment.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -4,3 +4,9 @@ let continent = "North America";
 let population = 332000000;
 
 console.log(country, continent, population);
+/////////////////////////////////////////////////////////////////
+// Data Types
+let isIsland = false;
+let language;
+
+console.log(isIsland, population, country, language);
