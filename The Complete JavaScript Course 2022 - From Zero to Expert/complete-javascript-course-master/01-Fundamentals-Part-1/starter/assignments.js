eaeb654ec882282726b641a1fb84d76d3f163de9
Dice-K39 <daisukehashimoto39@gmail.com>
2022-03-16 11:37:31 -0400

    Completed assignment for equality operators.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -29,3 +29,19 @@ console.log(population < averagePopulation);
 // Strings and Template Literals
 const description = `${country} is in ${continent}, and its ${population} million people speak ${language}.`
 console.log(description);
+/////////////////////////////////////////////////////////////////
+// Equality Operators: == vs ===
+const numNeighbors = Number(prompt("How many neighbor countries does your country have?"));
+
+if (numNeighbors === 1)
+{
+    console.log("Only 1 border!");
+}
+else if (numNeighbors >= 2)
+{
+    console.log("More than 1 border!");
+}
+else
+{
+    console.log("No borders.");
+}
