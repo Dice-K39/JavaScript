febc25932a488445ffe583980b5b6ec1f8de88e4
Dice-K39 <daisukehashimoto39@gmail.com>
2022-03-18 08:23:14 -0400

    Reviewed about strict mode.

---
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+console.log("------------ Lecture ------------");
+// Activating Strict Mode
+// Always first line - helps in debugging
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest)
+{
+    hasDriversLicense = true;
+}
+
+if (hasDriversLicense)
+{
+    console.log("I can drive");
+}
+
+// reserved keywords in programming language
+// const interface = "Audio";
+// const private = 534;
+// const if = 21;
