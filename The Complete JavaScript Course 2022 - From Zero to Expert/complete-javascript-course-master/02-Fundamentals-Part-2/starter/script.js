a08952dbc8b81496ab7498176a5169863e71f8cb
Dice-K39 <daisukehashimoto39@gmail.com>
2022-03-18 08:37:10 -0400

    Reviewed functions.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 console.log("------------ Lecture ------------");
+/*
 // Activating Strict Mode
 // Always first line - helps in debugging
 let hasDriversLicense = false;
@@ -8,7 +9,7 @@ const passTest = true;
 
 if (passTest)
 {
-    hasDriversLicense = true;
+    hasDriverLicense = true;
 }
 
 if (hasDriversLicense)
@@ -20,3 +21,31 @@ if (hasDriversLicense)
 // const interface = "Audio";
 // const private = 534;
 // const if = 21;
+/////////////////////////////////////////////////////////////////
+*/
+// Functions
+function logger()
+{
+    console.log("My name is Dice");
+}
+
+// calling/running/invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges)
+{
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number("23");
