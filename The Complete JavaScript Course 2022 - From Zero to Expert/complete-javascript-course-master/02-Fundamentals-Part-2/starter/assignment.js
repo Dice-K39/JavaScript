bd5abea5a54d785417fc61abff28d5421f30b11f
Dice-K39 <daisukehashimoto39@gmail.com>
2022-03-18 09:17:29 -0400

    Committing first part of assignment for declarations and expressions.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/02-Fundamentals-Part-2/starter/assignment.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/02-Fundamentals-Part-2/starter/assignment.js
@@ -1,6 +1,7 @@
 "use strict";
 
 console.log("------------ Assignment ------------");
+/*
 // Functions
 function describeCountry(country, population, capitalCity)
 {
@@ -14,3 +15,20 @@ const canada = describeCountry("Canada", 38.01, "Ottawa");
 console.log(usa);
 console.log(japan);
 console.log(canada);
+/////////////////////////////////////////////////////////////////
+*/
+// Function Declarations vs Expressions
+function percentageOfWorld1(population)
+{
+    return Number(((population / 7900) * 100).toFixed(2));
+}
+
+const usa = percentageOfWorld1(323);
+const japan = percentageOfWorld1(125.8);
+const canada = percentageOfWorld1(38.01);
+const china = percentageOfWorld1(1441);
+
+console.log(usa);
+console.log(japan);
+console.log(canada);
+console.log(china);
