8345d4a2c1c5d4eb449481cbec306aa6499c41e8
Dice-K39 <daisukehashimoto39@gmail.com>
2022-03-21 14:24:16 -0400

    Completed assignment on the for loop.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/02-Fundamentals-Part-2/starter/assignment.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/02-Fundamentals-Part-2/starter/assignment.js
@@ -140,3 +140,8 @@ myCountry.describe();
 console.log(myCountry.checkIsland());
 /////////////////////////////////////////////////////////////////
 */
+// Iteration: The for Loop
+for (let voter = 1; voter <= 50; voter++)
+{
+    console.log(`Voter number ${voter} is currently voting`);
+}
