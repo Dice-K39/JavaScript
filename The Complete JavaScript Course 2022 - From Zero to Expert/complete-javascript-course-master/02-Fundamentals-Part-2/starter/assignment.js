05a14f9e7c14af4cdd47386327434862678bca1a
Dice-K39 <daisukehashimoto39@gmail.com>
2022-03-21 15:09:01 -0400

    Completed assignment on looping backwards and loops inside loops.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/02-Fundamentals-Part-2/starter/assignment.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/02-Fundamentals-Part-2/starter/assignment.js
@@ -162,3 +162,18 @@ for (let i = 0; i < populations.length; i++)
 console.log(percentages);
 /////////////////////////////////////////////////////////////////
 */
+// Looping Backwards and Loops in Loops
+const listOfNeighbors = 
+[
+    ["Canada", "Mexico"],
+    ["Spain"],
+    ["Norway", "Sweden", "Russia"]
+]
+
+for (let neighbors = 0; neighbors < listOfNeighbors.length; neighbors++)
+{
+    for (let country = 0; country < listOfNeighbors[neighbors].length; country++)
+    {
+        console.log(`Neighbor: ${listOfNeighbors[neighbors][country]}`)
+    }
+}
