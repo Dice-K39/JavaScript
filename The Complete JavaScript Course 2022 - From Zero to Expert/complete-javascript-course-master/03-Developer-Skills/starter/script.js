61fb978e1233594aecbf65fcebffd6ffd7e57585
Dice-K39 <daisukehashimoto39@gmail.com>
2022-03-22 11:18:33 -0400

    Learning how to set up VS Code with useful extensions.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -1,3 +1,11 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+const x = 23;
+
+function example() {
+    console.log("Hi");
+}
+
+const calcAge = (birthYear) => 2037 - birthYear;
+console.log();
