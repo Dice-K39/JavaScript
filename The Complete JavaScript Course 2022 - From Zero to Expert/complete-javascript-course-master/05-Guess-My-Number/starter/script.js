8fa17a1d5e4b6958a5ea1a2f5ab0ca2658c54cbf
Dice-K39 <daisukehashimoto39@gmail.com>
2022-03-23 16:38:39 -0400

    Completed coding challenge of implementing reset button on project.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -13,8 +13,6 @@ console.log(document.querySelector(".guess").value);
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
-document.querySelector(".number").textContent = secretNumber;
-
 document.querySelector(".check").addEventListener("click", function () {
     const guess = Number(document.querySelector(".guess").value);
     console.log(guess, typeof guess);
@@ -26,6 +24,8 @@ document.querySelector(".check").addEventListener("click", function () {
     // when guess is correct
     else if (guess === secretNumber) {
         document.querySelector(".message").textContent = "✅ Correct Number!";
+
+        document.querySelector(".number").textContent = secretNumber;
 
         document.querySelector("body").style.backgroundColor = "#60b347";
 
@@ -55,4 +55,16 @@ document.querySelector(".check").addEventListener("click", function () {
             document.querySelector(".score").textContent = 0;
         }
     }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+    document.querySelector("body").style.backgroundColor = "#222";
+    document.querySelector(".number").textContent = "?";
+    document.querySelector(".number").style.width = "15rem";
+    document.querySelector(".guess").value = "";
+    document.querySelector(".message").textContent = "Start guessing...";
+    document.querySelector(".score").textContent = score;
 });
