5969e8540e66c3efc11e06f6b6dcba57c0db7e7a
Dice-K39 <daisukehashimoto39@gmail.com>
2022-03-24 15:19:14 -0400

    Implemented reset button. Completed third project.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -12,6 +12,8 @@ const current1El = document.getElementById("current--1");
 const player0El = document.querySelector(".player--0");
 const player1El = document.querySelector(".player--1");
 
+let scores, currentScore, activePlayer, playing;
+
 const switchPlayer = function () {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
     currentScore = 0;
@@ -20,15 +22,26 @@ const switchPlayer = function () {
     player1El.classList.toggle("player--active");
 };
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    diceEl.classList.add("hidden");
+    player0El.classList.remove("player--winner");
+    player1El.classList.remove("player--winner");
+    player0El.classList.add("player--active");
+    player1El.classList.remove("player--active");
+};
 
 // Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
+init();
 
 // Rolling dice functionality
 btnRoll.addEventListener("click", function () {
@@ -61,7 +74,7 @@ btnHold.addEventListener("click", function () {
             scores[activePlayer];
 
         // 2. Check if player's score is >= 100
-        if (scores[activePlayer] >= 20) {
+        if (scores[activePlayer] >= 100) {
             // Finish the game
             playing = false;
             diceEl.classList.add("hidden");
@@ -77,3 +90,5 @@ btnHold.addEventListener("click", function () {
         }
     }
 });
+
+btnNew.addEventListener("click", init);
