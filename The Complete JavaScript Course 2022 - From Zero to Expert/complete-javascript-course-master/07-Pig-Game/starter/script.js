c24f209e317f97c6da49773685ea78d98e3ea074
Dice-K39 <daisukehashimoto39@gmail.com>
2022-03-24 14:26:44 -0400

    Implemented code to switch active player.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -9,8 +9,12 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Starting conditions
 score0El.textContent = 0;
@@ -30,8 +34,14 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
