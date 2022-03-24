eb6ab551191b73d9af775be06a9a3468fc6e2347
Dice-K39 <daisukehashimoto39@gmail.com>
2022-03-24 11:30:29 -0400

    Completed project with full functionality.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -10,8 +10,9 @@ document.querySelector(".score").textContent = 10;
 document.querySelector(".guess").value = 23;
 console.log(document.querySelector(".guess").value);
 */
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
     const guess = Number(document.querySelector(".guess").value);
@@ -23,6 +24,11 @@ document.querySelector(".check").addEventListener("click", function () {
     }
     // when guess is correct
     else if (guess === secretNumber) {
+        if (score > highScore) {
+            highScore = score;
+
+            document.querySelector(".highscore").textContent = highScore;
+        }
         document.querySelector(".message").textContent = "✅ Correct Number!";
 
         document.querySelector(".number").textContent = secretNumber;
