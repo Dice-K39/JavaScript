d25b0422b761e5f77f8f63692f1b49bb044f0124
Dice-K39 <daisukehashimoto39@gmail.com>
2022-03-31 16:55:25 -0400

    Review of hoisting and the temporal dead zone.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -1,5 +1,5 @@
 "use strict";
-
+/*
 // Scoping in Practice
 function calcAge(birthYear) {
     const age = 2037 - birthYear;
@@ -39,3 +39,52 @@ const firstName = "Dice";
 calcAge(1991);
 // console.log(age);
 // printAge();
+/////////////////////////////////////////////////////////////////
+*/
+// Hoisting and TDZ in Practice
+
+// Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = "Dice";
+let job = "programmer";
+const year = 1991;
+
+// Functions
+console.log(addDeclaration(2, 3));
+// console.log(addExpression(2, 3));
+console.log(addArrow);
+// console.log(addArrow(2, 3));
+
+function addDeclaration(a, b) {
+    return a + b;
+}
+
+const addExpression = function (a, b) {
+    return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+console.log(numProducts);
+
+if (!numProducts) {
+    deleteShoppingCart();
+}
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+    console.log("All products deleted!");
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
