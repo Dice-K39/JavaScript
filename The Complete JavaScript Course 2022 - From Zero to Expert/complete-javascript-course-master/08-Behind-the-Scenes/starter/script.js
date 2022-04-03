a91644d6c64f8db4a58cf1511dd1f96dfa24de68
Dice-K39 <daisukehashimoto39@gmail.com>
2022-04-03 12:05:19 -0400

    Reviewed primitive and reference type behavior.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -122,7 +122,6 @@ paul.calcAge();
 const f = dice.calcAge;
 f();
 /////////////////////////////////////////////////////////////////
-*/
 // Regular Functions vs. Arrow Functions
 // var firstName = "Matt";
 
@@ -169,3 +168,58 @@ var addArrow = (a, b) => {
     return a + b;
 };
 addArrow(2, 5, 8);
+/////////////////////////////////////////////////////////////////
+// Primitives vs. Objects (Primitive vs. Reference Types)
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+const me = {
+    name: "Dice",
+    age: 30,
+};
+const friend = me;
+friend.age = 27;
+console.log("Friend:", friend);
+console.log("Me:", me);
+/////////////////////////////////////////////////////////////////
+*/
+// Primitives vs. Objects in Practice
+// Primitive types
+let lastName = "Williams";
+let oldLastName = lastName;
+lastName = "Davis";
+console.log(lastName, oldLastName);
+
+// Reference types
+const jessica = {
+    firstName: "Jessica",
+    lastName: "Williams",
+    age: 27,
+};
+const marriedJessica = jessica;
+marriedJessica.lastName = "Davis";
+console.log("Before marriage:", jessica);
+console.log("After marriage:", marriedJessica);
+
+// marriedJessica = {};
+
+// Copyihng objects
+const jessica2 = {
+    firstName: "Jessica",
+    lastName: "Williams",
+    age: 27,
+    family: ["Alice", "Bob"],
+};
+
+const jessicaCopy = Object.assign({}, jessica2); // shallow copy: only copies first level properties
+jessicaCopy.lastName = "Davis";
+console.log("Before marriage:", jessica2);
+console.log("After marriage:", jessicaCopy);
+
+jessicaCopy.family.push("Mary");
+jessicaCopy.family.push("John");
+console.log("Before marriage:", jessica2);
+console.log("After marriage:", jessicaCopy);
