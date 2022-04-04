f9dbb7ce1a6c00c61125b3647da638608bdab8da
Dice-K39 <daisukehashimoto39@gmail.com>
2022-04-04 12:36:54 -0400

    Revieweed the nullish coalescing operator.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -53,6 +53,17 @@ const restaurant = {
     },
 };
 
+// The Nullish Coalescing Operator (??)
+restaurant.numGuests = 0;
+const guests = restaurant.numGuests || 10;
+console.log(guests);
+
+// Nullish: null and undefined (NOT 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
+
+/*
+/////////////////////////////////////////////////////////////////
 // Short Circuiting (&& and ||)
 // Logical operators can:
 // 1 use any data type,
@@ -85,7 +96,6 @@ if (restaurant.orderPizza) {
 }
 
 restaurant.orderPizza && restaurant.orderPizza("mushrooms", "spinich");
-/*
 /////////////////////////////////////////////////////////////////
 // Rest Pattern and Parameters
 // 1) Destructuring
