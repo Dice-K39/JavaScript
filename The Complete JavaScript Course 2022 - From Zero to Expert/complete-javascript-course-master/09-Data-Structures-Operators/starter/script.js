d64e756e72003034d544a47649578c639d7a28be
Dice-K39 <daisukehashimoto39@gmail.com>
2022-04-04 12:19:03 -0400

    Reviewed rest operator and parameter.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -47,8 +47,50 @@ const restaurant = {
             `Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}`
         );
     },
-};
 
+    orderPizza: function (mainIngredient, ...otherIngredients) {
+        console.log(mainIngredient, otherIngredients);
+    },
+};
+// Rest Pattern and Parameters
+// 1) Destructuring
+// SPREAD: used on RIGHT of assignment
+const arr = [1, 2, ...[3, 4]];
+
+// REST: used on LEFT of assignment; only one in any destructuring
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [pizza, , risotto, ...otherFood] = [
+    ...restaurant.mainMenu,
+    ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood);
+
+// Objects
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
+
+// 2) Functions
+const add = function (...numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+    console.log(sum);
+};
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza("mushrooms", "onion", "olives", "spinich");
+restaurant.orderPizza("mushrooms");
+
+/*
+/////////////////////////////////////////////////////////////////
 // The Spread Operator (...)
 const arr = [7, 8, 9];
 const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
@@ -96,7 +138,6 @@ const restaurantCopy = { ...restaurant };
 restaurantCopy.name = "My Restaurant";
 console.log(restaurantCopy.name);
 console.log(restaurant.name);
-/*
 /////////////////////////////////////////////////////////////////
 restaurant.orderDelivery({
     time: "22:30",
