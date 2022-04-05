924d0997e7796b4c3be66ba7582008a70f072069
Dice-K39 <daisukehashimoto39@gmail.com>
2022-04-05 11:54:33 -0400

    Reviewed looping objects with for-of and the keys, values, and entries methods.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -56,6 +56,32 @@ const restaurant = {
         console.log(mainIngredient, otherIngredients);
     },
 };
+// Looping Objects: Object Keys, Values, and Entries
+
+// Property names
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days: `;
+
+for (const day of properties) {
+    openStr += `${day}, `;
+}
+console.log(openStr);
+
+// Property values
+const values = Object.values(openingHours);
+console.log(values);
+
+// Entire object
+const entries = Object.entries(openingHours);
+// console.log(entries);
+
+for (const [day, { open, close }] of entries) {
+    console.log(`On ${day} we open at ${open} and close at ${close}`);
+}
+/*
+/////////////////////////////////////////////////////////////////
 // Optional Chaining (?.)
 if (restaurant.openingHours && restaurant.openingHours.mon) {
     console.log(restaurant.openingHours.mon.open);
@@ -89,7 +115,6 @@ if (users.length > 0) {
 } else {
     console.log("user array empty");
 }
-/*
 /////////////////////////////////////////////////////////////////
 // Looping Arrays: The for-of Loop
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
