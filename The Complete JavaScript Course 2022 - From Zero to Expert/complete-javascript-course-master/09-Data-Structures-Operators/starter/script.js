ed87050034b2ef0dbf6035450b592d37710fd2ee
Dice-K39 <daisukehashimoto39@gmail.com>
2022-04-06 11:55:55 -0400

    Reviewed important string methods.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -56,6 +56,55 @@ const restaurant = {
         console.log(mainIngredient, otherIngredients);
     },
 };
+// Working with Strings - Part 3
+// Split and join
+console.log("a+very+nice+string".split("+"));
+console.log("Dice Hash".split(" "));
+
+const [firstName, lastName] = "Dice Hash".split(" ");
+
+const newName = ["Mr.", firstName, lastName.toUpperCase()].join(" ");
+console.log(newName);
+
+const capitalizeName = function (name) {
+    const names = name.split(" ");
+    const namesUpper = [];
+
+    for (const n of names) {
+        // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+        namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+    }
+    console.log(namesUpper.join(" "));
+};
+capitalizeName("jessica ann smith davis");
+capitalizeName("dice hash");
+
+// Padding
+const message = "Go to gate 23";
+console.log(message.padStart(25, "+").padEnd(30, "+"));
+console.log("Dice".padStart(20, "+").padEnd(30, "+"));
+
+const maskCreditCard = function (number) {
+    const str = number + "";
+    const last = str.slice(-4);
+    return last.padStart(str.length, "*");
+};
+console.log(maskCreditCard(75674784));
+console.log(maskCreditCard(46548523421546546546));
+console.log(maskCreditCard("456845646216513287897954"));
+
+// Repeat
+const message2 = "Bad weather... All departures Delayed... ";
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+    console.log(`There are ${n} planes in line ${"🛩 ".repeat(n)}`);
+};
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+/*
+/////////////////////////////////////////////////////////////////
 // Working with Strings - Part 2
 const airline = "Delta Airline Company";
 
@@ -115,7 +164,6 @@ const checkBaggage = function (items) {
 checkBaggage("I have a laptop, some Food, and a pocket Knife");
 checkBaggage("Socks and camera");
 checkBaggage("Got some snacks and a gun for protection");
-/*
 /////////////////////////////////////////////////////////////////
 // Working with Strings - Part 1
 const airline = "Delta Airline Company";
