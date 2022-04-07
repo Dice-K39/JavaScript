c896dc7874bcacf8c612c770b608530f9af51e29
Dice-K39 <daisukehashimoto39@gmail.com>
2022-04-07 11:22:17 -0400

    Reviewed passing arguments by value and reference.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/10-Functions/starter/script.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/10-Functions/starter/script.js
@@ -2,6 +2,8 @@
 
 console.log("--------------- Lecture ---------------");
 
+/*
+/////////////////////////////////////////////////////////////////
 // Default Parameters
 const bookings = [];
 
@@ -28,3 +30,37 @@ createBooking("XYZ098", 2, 800);
 createBooking("ABC123", 2);
 createBooking("ABC123", 5);
 createBooking("ABC123", undefined, 1000);
+/////////////////////////////////////////////////////////////////
+*/
+// How Passing Arguments Works: Value vs Reference
+const flight = "ABC123";
+const dice = {
+    name: "Dice Hash",
+    passport: 1234567890,
+};
+
+const checkIn = function (flightNum, passenger) {
+    flightNum = "XYZ098";
+    passenger.name = "Mr. " + passenger.name;
+
+    if (passenger.passport === 1234567890) {
+        alert("Checked in");
+    } else {
+        alert("Wrong passport");
+    }
+};
+
+// checkIn(flight, dice);
+// console.log(flight);
+// console.log(dice);
+
+// Is the same as doing...
+// const flightNum = flight;
+// const passenger = dice;
+
+const newPassport = function (person) {
+    person.passport = Math.trunc(Math.random() * 1000000000);
+};
+
+newPassport(dice);
+checkIn(flight, dice);
