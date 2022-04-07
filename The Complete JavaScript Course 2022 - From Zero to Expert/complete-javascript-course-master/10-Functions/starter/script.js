1aed41ae84b188d50233a5e031e6d8eca03af990
Dice-K39 <daisukehashimoto39@gmail.com>
2022-04-07 11:06:36 -0400

    Reviewed default parameters.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/10-Functions/starter/script.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/10-Functions/starter/script.js
@@ -1,1 +1,30 @@
-'use strict';
+"use strict";
+
+console.log("--------------- Lecture ---------------");
+
+// Default Parameters
+const bookings = [];
+
+const createBooking = function (
+    flightNum,
+    numPassengers = 1,
+    price = 199 * numPassengers
+) {
+    // ES5
+    // numPassengers ||= 1;
+    // price ||= 199;
+
+    const booking = {
+        flightNum,
+        numPassengers,
+        price,
+    };
+    console.log(booking);
+    bookings.push(booking);
+};
+
+createBooking("ABC123");
+createBooking("XYZ098", 2, 800);
+createBooking("ABC123", 2);
+createBooking("ABC123", 5);
+createBooking("ABC123", undefined, 1000);
