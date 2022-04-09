f870ed5b987f5ba3015ee8358f223cdc88895a99
Dice-K39 <daisukehashimoto39@gmail.com>
2022-04-09 13:01:26 -0400

    Learned about the new at method from ES2022.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -76,7 +76,8 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 /////////////////////////////////////////////////
 
 console.log("--------------- Lecture ---------------");
-
+/*
+/////////////////////////////////////////////////////////////////
 // Simple Array Methods
 let arr = ["a", "b", "c", "d", "e"];
 
@@ -113,3 +114,17 @@ console.log([...arr, ...arr2]);
 
 // Join
 console.log(letters.join("-"));
+/////////////////////////////////////////////////////////////////
+*/
+// The New at Method - ES2022
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+// getting the last array element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+
+console.log("dice".at(0));
+console.log("dice".at(-1));
