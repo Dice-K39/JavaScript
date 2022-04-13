2fad16b9146c0e18e045442a92f479c7557a84b3
Dice-K39 <daisukehashimoto39@gmail.com>
2022-04-13 10:39:20 -0400

    Completed coding challenge 3.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/11-Arrays-Bankist/starter/coding-challenge.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/11-Arrays-Bankist/starter/coding-challenge.js
@@ -40,3 +40,12 @@ console.log(calcAverageHumanAge(data1));
 console.log(calcAverageHumanAge(data2));
 /////////////////////////////////////////////////////////////////
 */
+// Coding Challenge 3
+const data1 = [5, 2, 4, 1, 15, 8, 3];
+const data2 = [16, 6, 10, 5, 6, 1, 4];
+
+const calcAverageHumanAge = (dogAges) => dogAges.map((age) => age <= 2 ? 2 * age : 16 + age * 4)
+        .filter((humanAge) => humanAge >= 18)
+        .reduce((acc, dogAge, _, arr) => acc + dogAge / arr.length, 0);
+console.log(calcAverageHumanAge(data1));
+console.log(calcAverageHumanAge(data2));
