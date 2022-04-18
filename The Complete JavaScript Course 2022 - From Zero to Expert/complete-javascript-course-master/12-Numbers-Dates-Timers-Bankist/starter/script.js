ff63137fb9dfa4446436b842cbde51d7683174c5
Dice-K39 <daisukehashimoto39@gmail.com>
2022-04-18 14:44:21 -0400

    Implemented a function on project that calculates dates that produces strings indicating when a transaction occured.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -20,10 +20,10 @@ const account1 = {
 		'2019-12-23T07:42:02.383Z',
 		'2020-01-28T09:15:04.904Z',
 		'2020-04-01T10:17:24.185Z',
-		'2020-05-08T14:11:59.604Z',
-		'2020-05-27T17:01:17.194Z',
-		'2020-07-11T23:36:17.929Z',
-		'2020-07-12T10:51:36.790Z'
+		'2022-04-10T14:11:59.604Z',
+		'2022-04-12T17:01:17.194Z',
+		'2022-04-15T23:36:17.929Z',
+		'2022-04-17T10:51:36.790Z'
 	],
 	currency: 'EUR',
 	locale: 'pt-PT' // de-DE
@@ -80,6 +80,30 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 /////////////////////////////////////////////////
 // Functions
+const formatMovementDate = function (date) {
+	const calcDaysPassed = (date1, date2) =>
+		Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+	const daysPassed = calcDaysPassed(new Date(), date);
+	console.log(daysPassed);
+
+	if (daysPassed === 0) {
+		return 'Today';
+	}
+
+	if (daysPassed === 1) {
+		return 'Yesterday';
+	}
+
+	if (daysPassed <= 7) {
+		return `${daysPassed} days ago`;
+	} else {
+		const month = `${date.getMonth() + 1}`.padStart(2, 0);
+		const day = `${date.getDate()}`.padStart(2, 0);
+		const year = date.getFullYear();
+
+		return `${month}/${day}/${year}`;
+	}
+};
 const displayMovements = function (acc, sort = false) {
 	containerMovements.innerHTML = '';
 
@@ -90,10 +114,7 @@ const displayMovements = function (acc, sort = false) {
 	movs.forEach(function (movement, i) {
 		const type = movement > 0 ? 'deposit' : 'withdrawal';
 		const date = new Date(acc.movementsDates[i]);
-		const month = `${date.getMonth() + 1}`.padStart(2, 0);
-		const day = `${date.getDate()}`.padStart(2, 0);
-		const year = date.getFullYear();
-		const displayDate = `${month}/${day}/${year}`;
+		const displayDate = formatMovementDate(date);
 
 		const html = `
         <div class="movements__row">
@@ -472,3 +493,15 @@ future.setFullYear(2040);
 console.log(future);
 /////////////////////////////////////////////////////////////////
 */
+// Operations with Dates
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(+future);
+
+const calcDaysPassed = (date1, date2) =>
+	Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+
+const days1 = calcDaysPassed(
+	new Date(2037, 3, 4),
+	new Date(2037, 3, 14, 10, 8)
+);
+console.log(days1);
