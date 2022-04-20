d160c8622308ac78930e04553fe7dbad75545f86
Dice-K39 <daisukehashimoto39@gmail.com>
2022-04-20 14:38:31 -0400

    Implemented smotth scrolling for a section on project.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -30,9 +30,45 @@ document.addEventListener('keydown', function (e) {
 	}
 });
 
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+	const s1coords = section1.getBoundingClientRect();
+	console.log(s1coords);
+
+	console.log(e.target.getBoundingClientRect());
+
+	console.log(
+		'Current scroll (X/Y)',
+		window.pageXOffset,
+		window.AnimationTimelinepageYOffset
+	);
+
+	console.log(
+		'height/width viewport',
+		document.documentElement.clientHeight,
+		document.documentElement.clientWidth
+	);
+
+	// Scrolling
+	// window.scrollTo(
+	// 	s1coords.left + window.pageXOffset,
+	// 	s1coords.top + window.pageYOffset
+	// );
+
+	// window.scrollTo({
+	// 	left: s1coords.left + window.pageXOffset,
+	// 	top: s1coords.top + window.pageYOffset,
+	// 	behavior: 'smooth'
+	// });
+
+	section1.scrollIntoView({ behavior: 'smooth' });
+});
+
 /////////////////////////////////////////////////////////////////
 console.log('--------------- Lecture ---------------');
-
+/*
 // Selecting, Creating, and Deleting Elements
 console.log(document.documentElement);
 console.log(document.head);
@@ -117,3 +153,5 @@ log.classList.contains('c'); // not includes like array
 
 // Dont use
 logo.className = 'dice';
+/////////////////////////////////////////////////////////////////
+*/
