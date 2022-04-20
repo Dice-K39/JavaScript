dcaeafe8eb70407a1b3b8efc54798b8dd66addf1
Dice-K39 <daisukehashimoto39@gmail.com>
2022-04-20 13:42:17 -0400

    Reviewed how to select, create, and delete elements.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -9,23 +9,63 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
 const openModal = function (e) {
-    e.preventDefault();
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
+	e.preventDefault();
+	modal.classList.remove('hidden');
+	overlay.classList.remove('hidden');
 };
 
 const closeModal = function () {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
+	modal.classList.add('hidden');
+	overlay.classList.add('hidden');
 };
 
-btnsOpenModal.forEach(btn => btn.addEventListener("click", openModal));
+btnsOpenModal.forEach((btn) => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', function (e) {
-    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-        closeModal();
-    }
+	if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+		closeModal();
+	}
 });
+
+/////////////////////////////////////////////////////////////////
+console.log('--------------- Lecture ---------------');
+
+// Selecting, Creating, and Deleting Elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section'); // returns NodeList
+console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button'); // returns HTMLCollection
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn')); // returns HTMLCollection
+
+// Creating and inserting elements
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent =
+// 	'We use cookies for improved functionality and analytics.';
+message.innerHTML =
+	'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+
+// header.before(message);
+// header.after(message);
+
+// Delete elements
+document
+	.querySelector('.btn--close-cookie')
+	.addEventListener('click', function (e) {
+		// message.remove();
+		message.parentElement.removeChild(message);
+	});
