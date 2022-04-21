d37fae8a49e4e27d7b3c40d3bf6c2970985027ab
Dice-K39 <daisukehashimoto39@gmail.com>
2022-04-21 09:54:56 -0400

    Reviewed types of events and different ways to attach event handlers.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -155,3 +155,16 @@ log.classList.contains('c'); // not includes like array
 logo.className = 'dice';
 /////////////////////////////////////////////////////////////////
 */
+// Types of Events and Event Handlers
+const h1 = document.querySelector('h1');
+const alertH1 = function (e) {
+	alert('addEventListener: Great! You are reading the heading');
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
+// h1.onmouseenter = function (e) {
+// 	alert('addEventListener: Great! You are reading the heading');
+// };
