4434d3cadcdbacab4b331fe49841f57cf6c60dca
Dice-K39 <daisukehashimoto39@gmail.com>
2022-04-26 14:07:35 -0400

    Reviewed prototypal inheritance on built-in objects.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/14-OOP/starter/script.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/14-OOP/starter/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 console.log('--------------- Lecture ---------------');
-
+/////////////////////////////////////////////////////////////////
 // 208 - Constructor Functions and the new Operator
 const Person = function (firstName, birthYear) {
 	// Instance properties
@@ -28,7 +28,7 @@ console.log(matilda, jack);
 
 console.log(dice instanceof Person);
 
-// 209 Prototypes
+// 209 - Prototypes
 console.log(Person.prototype);
 Person.prototype.calcAge = function () {
 	console.log(2037 - this.birthYear);
@@ -49,3 +49,26 @@ console.log(dice.species, matilda.species);
 
 console.log(dice.hasOwnProperty('firstName'));
 console.log(dice.hasOwnProperty('species'));
+/////////////////////////////////////////////////////////////////
+// 211 - Prototypal Inheritance on Built-In Objects
+console.log(dice.__proto__);
+// Object.prototype (top of prototype chain)
+console.log(dice.__proto__.__proto__);
+console.log(dice.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 6, 4, 5, 6, 9, 3, 9];
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+	return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir((x) => x + 1);
