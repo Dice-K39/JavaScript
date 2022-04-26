97718894b4ddb9400276966981daf72b3edee915
Dice-K39 <daisukehashimoto39@gmail.com>
2022-04-26 11:54:24 -0400

    Reviewed contructor functions and the new operator.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/14-OOP/starter/script.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/14-OOP/starter/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+console.log('--------------- Lecture ---------------');
+
+// 208 - Constructor Functions and the new Operator
+const Person = function (firstName, birthYear) {
+	// Instance properties
+	this.firstName = firstName;
+	this.birthYear = birthYear;
+
+	// Never do this
+	// this.calcAge = function() {
+	//     console.log(2037 - this.birthYear);
+	// }
+};
+
+const dice = new Person('Dice', 1991);
+console.log(dice);
+
+// 1 New {} is created
+// 2 function is called, this = {}
+// 3 {} linked to prototype
+// 4 function automatically return {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+console.log(matilda, jack);
+
+console.log(dice instanceof Person);
