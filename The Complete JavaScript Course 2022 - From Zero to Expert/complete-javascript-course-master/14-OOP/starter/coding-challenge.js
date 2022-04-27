51eeed1e4b966e94a83bdac4a1d4b50d57b42142
Dice-K39 <daisukehashimoto39@gmail.com>
2022-04-27 11:25:09 -0400

    Completed coding challenge 2.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/14-OOP/starter/coding-challenge.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/14-OOP/starter/coding-challenge.js
@@ -38,3 +38,37 @@ mercedes.brake();
 mercedes.accelerate();
 /////////////////////////////////////////////////////////////////
 */
+// Coding Challenge 2
+// 1
+class CarCl {
+	constructor(make, speed) {
+		this.make = make;
+		this.speed = speed;
+	}
+
+	accelerate() {
+		console.log(`The ${this.make} increased its speed to ${(this.speed += 10)} km/h.`);
+	}
+
+	brake() {
+		console.log(`The ${this.make} decreased its speed to ${(this.speed -= 5)} km/h`);
+	}
+
+	// 2
+	get speedUS() {
+		return this.speed / 1.6;
+	}
+
+	// 3
+	set speedUS(speed) {
+		this.speed = speed * 1.6;
+	}
+}
+// 4
+const ford = new CarCl('Ford', 120);
+console.log(ford.speedUS);
+ford.accelerate();
+ford.accelerate();
+ford.brake();
+ford.speedUS = 50;
+console.log(ford);
