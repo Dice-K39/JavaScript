0f7300f57a05388ac644c469150abed8140b2215
Dice-K39 <daisukehashimoto39@gmail.com>
2022-04-27 10:56:55 -0400

    Reviewed Object.create.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/14-OOP/starter/script.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/14-OOP/starter/script.js
@@ -144,6 +144,7 @@ jessica.greet();
 const walter = new PersonCl('Walter White', 1965);
 
 PersonCl.hey();
+/*
 /////////////////////////////////////////////////////////////////
 // 214 - Setters and Getters
 const account = {
@@ -162,3 +163,28 @@ console.log(account.latest);
 
 account.latest = 50;
 console.log(account.movements);
+/////////////////////////////////////////////////////////////////
+*/
+// 216 - Object.create
+const PersonProto = {
+	calcAge() {
+		console.log(2037 - this.birthYear);
+	},
+
+	init(firstName, birthYear) {
+		this.firstName = firstName;
+		this.birthYear = birthYear;
+	}
+};
+
+const steven = Object.create(PersonProto);
+console.log(steven);
+steven.name = 'Steven';
+steven.birthYear = 2002;
+steven.calcAge();
+
+console.log(steven.__proto__ === PersonProto);
+
+const sarah = Object.create(PersonProto);
+sarah.init('Sarah', 1979);
+sarah.calcAge();
