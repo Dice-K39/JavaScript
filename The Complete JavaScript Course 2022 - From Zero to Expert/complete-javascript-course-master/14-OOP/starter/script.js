19b4fd2392c04fd1ff48e5214705866373a43a67
Dice-K39 <daisukehashimoto39@gmail.com>
2022-04-28 09:36:36 -0400

    Reviewed ES6 classes.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/14-OOP/starter/script.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/14-OOP/starter/script.js
@@ -286,7 +286,6 @@ const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
 martha.introduce();
 martha.calcAge();
 /////////////////////////////////////////////////////////////////
-*/
 // 221 - Inheritance Between "Classes": Object.create
 const PersonProto = {
 	calcAge() {
@@ -316,3 +315,49 @@ const jay = Object.create(StudentProto);
 jay.init('Jay', 2010, 'Computer Science');
 jay.introduce();
 jay.calcAge();
+/////////////////////////////////////////////////////////////////
+*/
+// 222 - Another Class Example
+class Account {
+	constructor(owner, currency, pin) {
+		this.owner = owner;
+		this.currency = currency;
+		this.pin = pin;
+		this.movements = [];
+		this.locale = navigator.language;
+
+		console.log(`Thanks for opening an account, ${owner}`);
+	}
+
+	// Public interface
+	deposit(value) {
+		this.movements.push(value);
+	}
+
+	withdraw(value) {
+		this.deposit(-value);
+	}
+
+	approveLoan(value) {
+		return true;
+	}
+
+	requestLoan(value) {
+		if (this.approveLoan(value)) {
+			this.deposit(value);
+			console.log('Loan approved');
+		}
+	}
+}
+
+const acc1 = new Account('Dice', 'USD', 1111);
+
+// acc1.movements.push(250);
+// acc1.movements.push(-140);
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+acc1.approveLoan(1000);
+
+console.log(acc1);
+console.log(acc1.pin);
