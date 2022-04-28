283ef49b0c39f4728bf8406cf7549198d3a4b126
Dice-K39 <daisukehashimoto39@gmail.com>
2022-04-28 09:48:25 -0400

    Separated topic and code  so that coding changes are more apparent.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/14-OOP/starter/script.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/14-OOP/starter/script.js
@@ -316,8 +316,57 @@ jay.init('Jay', 2010, 'Computer Science');
 jay.introduce();
 jay.calcAge();
 /////////////////////////////////////////////////////////////////
-*/
 // 222 - Another Class Example
+class Account {
+	constructor(owner, currency, pin) {
+		this.owner = owner;
+		this.currency = currency;
+		this.pin = pin;
+		this.movements = [];
+
+		this.locale = navigator.language;
+
+		console.log(`Thanks for opening an account, ${owner}`);
+	}
+
+	// Public interface
+	getMovements() {
+		return this.movements;
+	}
+
+	deposit(value) {
+		this.movements.push(value);
+	}
+
+	withdraw(value) {
+		this.deposit(-value);
+	}
+
+	approveLoan(value) {
+		return true;
+	}
+
+	requestLoan(value) {
+		if (this.approveLoan(value)) {
+			this.deposit(value);
+			console.log('Loan approved');
+		}
+	}
+}
+
+const acc1 = new Account('Dice', 'USD', 1111);
+
+// acc1._movements.push(250);
+// acc1._movements.push(-140);
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+acc1.approveLoan(1000);
+console.log(acc1.getMovements());
+console.log(acc1);
+console.log(acc1.pin);
+/////////////////////////////////////////////////////////////////
+*/
 // 223 - Encapsulation: Protected Properties and Methods
 class Account {
 	constructor(owner, currency, pin) {
