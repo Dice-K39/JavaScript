0135e768a26fae5e16edb9c478d1adc072ddf01c
Dice-K39 <daisukehashimoto39@gmail.com>
2022-05-06 13:21:45 -0400

    Reviewed error handling with try...catch.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -426,7 +426,6 @@ const whereAmI = function (lat, lng) {
 
 btn.addEventListener('click', whereAmI);
 /////////////////////////////////////////////////////////////////
-*/
 // 262 - Consuming Promises with Async/Await
 const renderCountry = function (data, className = '') {
 	const html = `
@@ -474,3 +473,82 @@ const whereAmI = async function () {
 };
 whereAmI();
 console.log('FIRST');
+/////////////////////////////////////////////////////////////////
+*/
+// 263 - Error Handling with try...catch
+const renderCountry = function (data, className = '') {
+	const html = `
+            <article class="country ${className}">
+                <img class="country__img" src="${data.flag}" />
+                <div class="country__data">
+                    <h3 class="country__name">${data.name}</h3>
+                    <h4 class="country__region">${data.region}</h4>
+                    <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(
+						1
+					)}</p>
+                    <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+                    <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+                </div>
+            </article>
+        `;
+
+	countriesContainer.insertAdjacentHTML('beforeend', html);
+	countriesContainer.style.opacity = 1;
+};
+
+const getPosition = function () {
+	return new Promise(function (resolve, reject) {
+		navigator.geolocation.getCurrentPosition(resolve, reject);
+	});
+};
+
+const renderError = function (msg) {
+	countriesContainer.insertAdjacentText('beforeend', msg);
+	// countriesContainer.style.opacity = 1;
+};
+
+// fetch(`https://restcountries.com/v2/name/${country}`).then(res => console.log(res))
+
+const whereAmI = async function () {
+	try {
+		// Geolocation
+		const pos = await getPosition();
+		const { latitude: lat, longitude: lng } = pos.coords;
+
+		// Reverse geocoding
+		const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+
+		if (!resGeo.ok) {
+			throw new Error('Problem getting location data');
+		}
+		const dataGeo = await resGeo.json();
+		console.log(dataGeo);
+
+		// Country data
+		const res = await fetch(`https://restcountries.com/v2/name/${dataGeo.country}`);
+
+		if (!res.ok) {
+			throw new Error('Problem getting location data');
+		}
+
+		const data = await res.json();
+		console.log(data);
+		renderCountry(data[0]);
+	} catch (err) {
+		console.error(err);
+		renderError(`Something went wrong ${err.message}`);
+	}
+};
+whereAmI();
+whereAmI();
+whereAmI();
+whereAmI();
+console.log('FIRST');
+
+// try {
+// 	let y = 1;
+// 	const x = 2;
+// 	y = 3;
+// } catch (err) {
+// 	alert(err.message);
+// }
