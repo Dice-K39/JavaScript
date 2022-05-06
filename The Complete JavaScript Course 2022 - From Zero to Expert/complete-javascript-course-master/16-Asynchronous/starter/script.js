69d415aa39e14aba34f36684dc787c0927031020
Dice-K39 <daisukehashimoto39@gmail.com>
2022-05-06 14:15:33 -0400

    Reviewed how to run promises in parallel.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -552,7 +552,6 @@ console.log('FIRST');
 // 	alert(err.message);
 // }
 /////////////////////////////////////////////////////////////////
-*/
 // 254 - Returning Values from Async Functions
 const renderCountry = function (data, className = '') {
 	const html = `
@@ -641,3 +640,35 @@ console.log('1: Will get location');
 
 	console.log('3: Finished getting location');
 })();
+/////////////////////////////////////////////////////////////////
+*/
+// Running Promises in Parallel
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+	return fetch(url).then((res) => {
+		if (!res.ok) {
+			throw new Error(`${errorMsg} (${res.status})`);
+		}
+
+		return res.json();
+	});
+};
+
+const get3Countries = async function (c1, c2, c3) {
+	try {
+		// const [data1] = await getJSON(`https://restcountries.com/v2/name/${c1}`);
+		// const [data2] = await getJSON(`https://restcountries.com/v2/name/${c2}`);
+		// const [data3] = await getJSON(`https://restcountries.com/v2/name/${c3}`);
+
+		// Promis.all combinator
+		const data = await Promise.all([
+			getJSON(`https://restcountries.com/v2/name/${c1}`),
+			getJSON(`https://restcountries.com/v2/name/${c2}`),
+			getJSON(`https://restcountries.com/v2/name/${c3}`)
+		]);
+
+		console.log(data.map((d) => d[0].capital));
+	} catch (err) {
+		console.error(err);
+	}
+};
+get3Countries('usa', 'canada', 'japan');
