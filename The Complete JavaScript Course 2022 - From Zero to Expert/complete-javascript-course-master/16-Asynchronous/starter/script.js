d672901c34b0e10d437e9e4181e734e7f5d0a1ab
Dice-K39 <daisukehashimoto39@gmail.com>
2022-05-06 13:52:33 -0400

    reviewed returning values from async functions.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -474,7 +474,6 @@ const whereAmI = async function () {
 whereAmI();
 console.log('FIRST');
 /////////////////////////////////////////////////////////////////
-*/
 // 263 - Error Handling with try...catch
 const renderCountry = function (data, className = '') {
 	const html = `
@@ -552,3 +551,93 @@ console.log('FIRST');
 // } catch (err) {
 // 	alert(err.message);
 // }
+/////////////////////////////////////////////////////////////////
+*/
+// 254 - Returning Values from Async Functions
+const renderCountry = function (data, className = '') {
+	const html = `
+            <article class="country ${className}">
+                <img class="country__img" src="${data.flag}" />
+                <div class="country__data">
+                    <h3 class="country__name">${data.name}</h3>
+                    <h4 class="country__region">${data.region}</h4>
+                    <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(
+						1
+					)}</p>
+                    <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+                    <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+                </div>
+            </article>
+        `;
+
+	countriesContainer.insertAdjacentHTML('beforeend', html);
+	countriesContainer.style.opacity = 1;
+};
+
+const getPosition = function () {
+	return new Promise(function (resolve, reject) {
+		navigator.geolocation.getCurrentPosition(resolve, reject);
+	});
+};
+
+const renderError = function (msg) {
+	countriesContainer.insertAdjacentText('beforeend', msg);
+	// countriesContainer.style.opacity = 1;
+};
+
+// fetch(`https://restcountries.com/v2/name/${country}`).then(res => console.log(res))
+
+const whereAmI = async function () {
+	try {
+		// Geolocation
+		const pos = await getPosition();
+		const { latitude: lat, longitude: lng } = pos.coords;
+
+		// Reverse geocoding
+		const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+
+		if (!resGeo.ok) {
+			throw new Error('Problem getting location data');
+		}
+		const dataGeo = await resGeo.json();
+
+		// Country data
+		const res = await fetch(`https://restcountries.com/v2/name/${dataGeo.country}`);
+
+		if (!res.ok) {
+			throw new Error('Problem getting location data');
+		}
+
+		const data = await res.json();
+
+		renderCountry(data[0]);
+
+		return `You are in ${dataGeo.city}, ${dataGeo.state}  ${dataGeo.country}`;
+	} catch (err) {
+		console.error(err);
+		renderError(`${err.message}`);
+
+		// Reject promise returned from async function
+		throw err;
+	}
+};
+console.log('1: Will get location');
+// const city = whereAmI();
+// console.log(city);
+
+// whereAmI()
+// 	.then((city) => console.log(`2: ${city}`))
+// 	.catch((err) => console.error(`2: ${err.message}`))
+// 	.finally(() => console.log('3: Finished getting location'));
+
+(async function () {
+	try {
+		const city = await whereAmI();
+
+		console.log(`2: ${city}`);
+	} catch (err) {
+		console.log(`2: ${err.message}`);
+	}
+
+	console.log('3: Finished getting location');
+})();
