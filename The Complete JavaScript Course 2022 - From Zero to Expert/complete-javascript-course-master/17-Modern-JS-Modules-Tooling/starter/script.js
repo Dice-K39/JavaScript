2f06d8e3421446e140277b0faac15fc822949d66
Dice-K39 <daisukehashimoto39@gmail.com>
2022-05-07 14:14:11 -0400

    Reviewed exporting and importing modules in ES6.

---
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -0,0 +1,23 @@
+console.log('--------------- Lecture ---------------');
+
+///////////////////////////////////////
+// 272 - Exporting and Importing in ES6 Modules
+// import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// addToCart('bread', 5);
+// console.log(price, tq);
+
+console.log('Importing module');
+// console.log(shippingCost);
+
+// import * as ShoppingCart from './shoppingCart.js';
+// ShoppingCart.addToCart('bread', 5);
+// console.log(ShoppingCart.totalPrice);
+
+// import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+
+import add, { cart } from './shoppingCart.js';
+add('pizza', 2);
+add('bread', 5);
+add('apples', 4);
+
+console.log(cart);
