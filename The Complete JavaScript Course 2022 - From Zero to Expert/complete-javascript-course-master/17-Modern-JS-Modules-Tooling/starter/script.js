7081e181ad805603dc5ef9d77abab140672605b8
Dice-K39 <daisukehashimoto39@gmail.com>
2022-05-09 10:08:49 -0400

    Reviewed the module pattern.

--- The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
+++ The Complete JavaScript Course 2022 - From Zero to Expert/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,5 +1,5 @@
 console.log('--------------- Lecture ---------------');
-
+/*
 ///////////////////////////////////////
 // 272 - Exporting and Importing in ES6 Modules
 // import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
@@ -44,3 +44,33 @@ console.log(lastPost);
 
 const lastPost2 = await getLastPost();
 console.log(lastPost2);
+///////////////////////////////////////
+*/
+// 274 - The Module Pattern
+const ShoppingCart2 = (function () {
+	const cart = [];
+	const shippingCost = 10;
+	const totalPrice = 237;
+	const totalQuantity = 23;
+
+	const addToCart = function (product, quantity) {
+		cart.push({ product, quantity });
+		console.log(`${quantity} ${product} added to cart (shipping cost is ${shippingCost})`);
+	};
+
+	const orderStock = function (product, quantity) {
+		console.log(`${quantity} ${product} ordered from supplier`);
+	};
+
+	return {
+		addToCart,
+		cart,
+		totalPrice,
+		totalQuantity
+	};
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);
