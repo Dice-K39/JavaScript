625c8b66b51e3cfcdf0f3d12638bae74b3e54881
Dice-K39 <daisukehashimoto39@gmail.com>
2022-06-01 08:23:58 -0400

    Reviewed how to handle form submission and used two-way binding.

--- React - The Complete Guide (incl Hooks, React Router, Redux)/Section 3 - React Basics & Working with Components/01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/Section 3 - React Basics & Working with Components/01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -74,6 +74,10 @@ const ExpenseForm = () => {
 		};
 
 		console.log(expenseData);
+
+		setEnteredTitle('');
+		setEnteredAmount('');
+		setEnteredDate('');
 	};
 
 	return (
@@ -84,6 +88,7 @@ const ExpenseForm = () => {
 					<input
 						type="text"
 						onChange={titleChangeHandler}
+						value={enteredTitle}
 					/>
 				</div>
 				<div className="new-expense__control">
@@ -93,6 +98,7 @@ const ExpenseForm = () => {
 						min="0.01"
 						step="0.01"
 						onChange={amountChangeHandler}
+						value={enteredAmount}
 					/>
 				</div>
 				<div className="new-expense__control">
@@ -102,6 +108,7 @@ const ExpenseForm = () => {
 						min="2019-01-01"
 						max="2022-12-31"
 						onChange={dateChangeHandler}
+						value={enteredDate}
 					/>
 				</div>
 			</div>
