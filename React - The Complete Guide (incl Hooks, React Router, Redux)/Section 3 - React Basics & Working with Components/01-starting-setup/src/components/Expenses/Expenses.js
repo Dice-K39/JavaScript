97bc6f8947da1236a631aac1d399ce85ff845797
Dice-K39 <daisukehashimoto39@gmail.com>
2022-06-04 07:53:14 -0400

    Added conditional return statements.

--- React - The Complete Guide (incl Hooks, React Router, Redux)/Section 3 - React Basics & Working with Components/01-starting-setup/src/components/Expenses/Expenses.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/Section 3 - React Basics & Working with Components/01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 
-import ExpenseFilter from './ExpensesFilter';
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpenseFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 import './Expenses.css';
 
 const Expenses = (props) => {
@@ -16,19 +16,6 @@ const Expenses = (props) => {
 		return item.date.getFullYear().toString() === yearFilter;
 	});
 
-	let expensesContent = <p>No expenses found.</p>;
-
-	if (filteredExpenses.length > 0) {
-		expensesContent = filteredExpenses.map((expense) => (
-			<ExpenseItem
-				key={expense.id}
-				title={expense.title}
-				amount={expense.amount}
-				date={expense.date}
-			/>
-		));
-	}
-
 	return (
 		<div>
 			<Card className="expenses">
@@ -36,7 +23,7 @@ const Expenses = (props) => {
 					selected={yearFilter}
 					onYearFilterChange={yearFilterChangeHandler}
 				/>
-				{expensesContent}
+				<ExpensesList items={filteredExpenses} />
 			</Card>
 		</div>
 	);
