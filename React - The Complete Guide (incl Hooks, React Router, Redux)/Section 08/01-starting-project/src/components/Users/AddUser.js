ff6bd2724f4d6f17b6ca39f4e7e8d3f2681e5cb0
Dice-K39 <daisukehashimoto39@gmail.com>
2022-06-10 07:40:01 -0400

    Managing user input with state.

--- React - The Complete Guide (incl Hooks, React Router, Redux)/Section 08/01-starting-project/src/components/Users/AddUser.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/Section 08/01-starting-project/src/components/Users/AddUser.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import classes from './AddUser.module.css';
 
 const AddUser = () => {
+	const [enteredUsername, setEnteredUsername] = useState('');
+	const [enteredAge, setEnteredAge] = useState('');
+
 	const addUserHandler = (e) => {
 		e.preventDefault();
+		console.log(enteredUsername, enteredAge);
+	};
+
+	const usernameChangeHandler = (e) => {
+		setEnteredUsername(e.target.value);
+	};
+
+	const ageChangeHandler = (e) => {
+		setEnteredAge(e.target.value);
 	};
 
 	return (
@@ -16,11 +28,13 @@ const AddUser = () => {
 				<input
 					id="username"
 					type="text"
+					onChange={usernameChangeHandler}
 				/>
 				<label htmlFor="age">Age (Years)</label>
 				<input
 					id="age"
 					type="number"
+					onChange={ageChangeHandler}
 				/>
 				<Button type="submit">Add User</Button>
 			</form>
