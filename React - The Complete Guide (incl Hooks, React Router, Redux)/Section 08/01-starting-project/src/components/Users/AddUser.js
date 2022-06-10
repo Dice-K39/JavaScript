b9a02713983c4b1645cc4bbf855a7cbc29f747a9
Dice-K39 <daisukehashimoto39@gmail.com>
2022-06-10 07:23:03 -0400

    Added a reusable Button component.

--- React - The Complete Guide (incl Hooks, React Router, Redux)/Section 08/01-starting-project/src/components/Users/AddUser.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/Section 08/01-starting-project/src/components/Users/AddUser.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Card from '../UI/Card';
+import Button from '../UI/Button';
 import classes from './AddUser.module.css';
 
 const AddUser = () => {
@@ -21,7 +22,7 @@ const AddUser = () => {
 					id="age"
 					type="number"
 				/>
-				<button type="submit">Add User</button>
+				<Button type="submit">Add User</Button>
 			</form>
 		</Card>
 	);
