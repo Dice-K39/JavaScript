b0fe0ab52786d35ab7d3538fc3b09304a6bd97a2
Dice-K39 <daisukehashimoto39@gmail.com>
2022-06-11 07:27:22 -0400

    Added validations and resetting logic.

--- React - The Complete Guide (incl Hooks, React Router, Redux)/Section 08/01-starting-project/src/components/Users/AddUser.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/Section 08/01-starting-project/src/components/Users/AddUser.js
@@ -10,7 +10,19 @@ const AddUser = () => {
 
 	const addUserHandler = (e) => {
 		e.preventDefault();
+
+		if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+			return;
+		}
+
+		if (+enteredAge < 1) {
+			return;
+		}
+
 		console.log(enteredUsername, enteredAge);
+
+		setEnteredUsername('');
+		setEnteredAge('');
 	};
 
 	const usernameChangeHandler = (e) => {
@@ -28,12 +40,14 @@ const AddUser = () => {
 				<input
 					id="username"
 					type="text"
+					value={enteredUsername}
 					onChange={usernameChangeHandler}
 				/>
 				<label htmlFor="age">Age (Years)</label>
 				<input
 					id="age"
 					type="number"
+					value={enteredAge}
 					onChange={ageChangeHandler}
 				/>
 				<Button type="submit">Add User</Button>
