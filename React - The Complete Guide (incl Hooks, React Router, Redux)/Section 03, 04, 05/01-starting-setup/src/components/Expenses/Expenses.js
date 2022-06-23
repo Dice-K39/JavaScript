6c0f2c318ff6cfa059f376fabc3caad964df6cf2
Dice-K39 <daisukehashimoto39@gmail.com>
2022-06-23 07:09:00 -0400

    Created a ExpensesList component to make JSX in Expenses.js leaner and added a conditonal return statement.

--- React - The Complete Guide (incl Hooks, React Router, Redux)/Section 03, 04, 05/01-starting-setup/src/components/Expenses/Expenses.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/Section 03, 04, 05/01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 
-import ExpenseItem from './ExpenseItem';
 import ExpensesFilter from './ExpensesFilter';
 import Card from '../UI/Card';
+import ExpensesList from './ExpensesList';
 
 import './Expenses.css';
 
@@ -17,26 +17,13 @@ const Expenses = (props) => {
 		return expense.date.getFullYear().toString() === filteredYear;
 	});
 
-	let expensesContent = <p>No expenses found.</p>;
-
-	if (filteredExpenses.length > 0) {
-		expensesContent = filteredExpenses.map((expense) => (
-			<ExpenseItem
-				key={expense.id}
-				title={expense.title}
-				amount={expense.amount}
-				date={expense.date}
-			/>
-		));
-	}
-
 	return (
 		<Card className="expenses">
 			<ExpensesFilter
 				onSelectedYearChange={selectedYearHandler}
 				selected={filteredYear}
 			/>
-			{expensesContent}
+			<ExpensesList items={filteredExpenses} />
 		</Card>
 	);
 };
