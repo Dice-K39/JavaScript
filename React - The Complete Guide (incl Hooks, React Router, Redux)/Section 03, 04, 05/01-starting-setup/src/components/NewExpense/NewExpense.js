6944b9a3946345caaa743af7b3e6fcb54bdbed97
Dice-K39 <daisukehashimoto39@gmail.com>
2022-06-23 07:43:59 -0400

    Added conditional content to NewExpenses component to display a button instead of a form and when pressed shows the form. If submitted or canceled, form is replaced with the single button.

--- React - The Complete Guide (incl Hooks, React Router, Redux)/Section 03, 04, 05/01-starting-setup/src/components/NewExpense/NewExpense.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/Section 03, 04, 05/01-starting-setup/src/components/NewExpense/NewExpense.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseForm from './ExpenseForm';
 
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+	const [isEditing, setIsEditing] = useState(false);
+
 	const saveExpenseDataHandler = (enteredExpenseData) => {
 		const expenseData = {
 			...enteredExpenseData,
@@ -14,9 +16,19 @@ const NewExpense = (props) => {
 		props.onAddExpense(expenseData);
 	};
 
+	const editingHandler = () => {
+		setIsEditing((prevState) => !prevState);
+	};
+
 	return (
 		<div className="new-expense">
-			<ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+			{!isEditing && <button onClick={editingHandler}>Add New Expense</button>}
+			{isEditing && (
+				<ExpenseForm
+					onSaveExpenseData={saveExpenseDataHandler}
+					onCancel={editingHandler}
+				/>
+			)}
 		</div>
 	);
 };
