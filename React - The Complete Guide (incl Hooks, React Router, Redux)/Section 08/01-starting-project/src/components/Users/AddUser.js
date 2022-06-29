7dac46cd97a7b968f7c221ef442d89e23dbdd857
Dice-K39 <daisukehashimoto39@gmail.com>
2022-06-29 07:25:49 -0400

    Added handlers for user input.

--- React - The Complete Guide (incl Hooks, React Router, Redux)/Section 08/01-starting-project/src/components/Users/AddUser.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/Section 08/01-starting-project/src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
@@ -6,8 +6,21 @@ import Button from '../UI/Button';
 import styles from './AddUser.module.css';
 
 const AddUser = (props) => {
+	const [enteredUsername, setEnteredUsername] = useState('');
+	const [enteredAge, setEnteredAge] = useState('');
+
 	const addUserHandler = (e) => {
 		e.preventDefault();
+
+		console.log(enteredUsername, enteredAge);
+	};
+
+	const usernameChangeHandler = (e) => {
+		setEnteredUsername(e.target.value);
+	};
+
+	const ageChangeHandler = (e) => {
+		setEnteredAge(e.target.value);
 	};
 
 	return (
@@ -17,11 +30,13 @@ const AddUser = (props) => {
 				<input
 					id="username"
 					type="text"
+					onChange={usernameChangeHandler}
 				/>
 				<label htmlFor="age">Age (Years)</label>
 				<input
 					id="age"
 					type="number"
+					onChange={ageChangeHandler}
 				/>
 				<Button type="submit">Add User</Button>
 			</form>
