11554c5e87256ffd703008424dd5488c0b853370
Dice-K39 <daisukehashimoto39@gmail.com>
2022-06-30 07:47:34 -0400

    Added an error state.

--- React - The Complete Guide (incl Hooks, React Router, Redux)/Section 08/01-starting-project/src/components/Users/AddUser.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/Section 08/01-starting-project/src/components/Users/AddUser.js
@@ -9,15 +9,26 @@ import styles from './AddUser.module.css';
 const AddUser = (props) => {
 	const [enteredUsername, setEnteredUsername] = useState('');
 	const [enteredAge, setEnteredAge] = useState('');
+	const [error, setError] = useState();
 
 	const addUserHandler = (e) => {
 		e.preventDefault();
 
 		if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+			setError({
+				title: 'Invalid input',
+				message: 'Please enter a valid name and age (non-empty values)'
+			});
+
 			return;
 		}
 
 		if (+enteredAge < 1) {
+			setError({
+				title: 'Invalid age',
+				message: 'Please enter a valid age (> 0)'
+			});
+
 			return;
 		}
 
