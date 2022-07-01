4acd35e75d022a39e040ace520dbd6098fb566dd
Dice-K39 <daisukehashimoto39@gmail.com>
2022-07-01 07:14:12 -0400

    Commented Wrapper import out to fix warning.

--- React - The Complete Guide (incl Hooks, React Router, Redux)/Section 09/01-starting-project/src/components/Users/AddUser.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/Section 09/01-starting-project/src/components/Users/AddUser.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import ErrorModal from '../UI/ErrorModal';
-import Wrapper from '../Helpers/Wrapper';
+// import Wrapper from '../Helpers/Wrapper';
 import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
