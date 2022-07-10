656f20bf808f502877f7b6af0440a187353ac10b
Dice-K39 <daisukehashimoto39@gmail.com>
2022-07-10 10:06:13 -0400

    Added styling to Header component.

--- React - The Complete Guide (incl Hooks, React Router, Redux)/Section 11/01-starting-project/src/components/Layout/Header.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/Section 11/01-starting-project/src/components/Layout/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
 import mealsImage from '../../assets/meals.jpeg';
+import classes from './Header.module.css';
 
 const Header = (props) => {
 	return (
 		<>
-			<header>
+			<header className={classes.header}>
 				<h1>ReactMeals</h1>
 				<button>Cart</button>
 			</header>
-			<div>
+			<div className={classes['main-image']}>
 				<img
 					src={mealsImage}
 					alt="A table full of delicious food!"
