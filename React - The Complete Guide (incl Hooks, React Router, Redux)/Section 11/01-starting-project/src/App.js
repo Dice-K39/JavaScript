2f74348c94b425a18482be54f11857c354383ed7
Dice-K39 <daisukehashimoto39@gmail.com>
2022-07-13 07:06:10 -0400

    Added state in App.js to show or hide Cart.

--- React - The Complete Guide (incl Hooks, React Router, Redux)/Section 11/01-starting-project/src/App.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/Section 11/01-starting-project/src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
 
 function App() {
+	const [cartIsVisible, setCartIsVisible] = useState(false);
+
+	const showCartHandler = () => {
+		setCartIsVisible(true);
+	};
+
+	const hideCartHandler = () => {
+		setCartIsVisible(false);
+	};
+
 	return (
 		<>
-			<Cart />
-			<Header />
+			{cartIsVisible && <Cart />}
+			<Header onShowCart={showCartHandler} />
 			<main>
 				<Meals />
 			</main>
