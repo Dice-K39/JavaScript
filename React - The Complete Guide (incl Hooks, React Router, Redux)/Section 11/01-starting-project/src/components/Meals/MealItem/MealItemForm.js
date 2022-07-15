fc53ff0200eac5f495a6adfebcc56a9e12878291
Dice-K39 <daisukehashimoto39@gmail.com>
2022-07-15 07:19:46 -0400

    Using useRef on Input custom component in MealItemForm.

--- React - The Complete Guide (incl Hooks, React Router, Redux)/Section 11/01-starting-project/src/components/Meals/MealItem/MealItemForm.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/Section 11/01-starting-project/src/components/Meals/MealItem/MealItemForm.js
@@ -1,11 +1,23 @@
+import { useRef } from 'react';
+
 import Input from '../../UI/Input';
 
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = (props) => {
+	const amountInputRef = useRef();
+
+	const submitHandler = (e) => {
+		e.preventDefault();
+	};
+
 	return (
-		<form className={classes.form}>
+		<form
+			className={classes.form}
+			onSubmit={submitHandler}
+		>
 			<Input
+				ref={amountInputRef}
 				label="Amount"
 				input={{
 					id: 'amount' + props.id,
