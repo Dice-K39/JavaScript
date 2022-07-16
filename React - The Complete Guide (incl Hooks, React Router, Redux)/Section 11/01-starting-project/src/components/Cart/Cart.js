58f96eff931ddbbe0b106ce103b104c00879f770
Dice-K39 <daisukehashimoto39@gmail.com>
2022-07-16 13:28:12 -0400

    Added context to Cart component.

--- React - The Complete Guide (incl Hooks, React Router, Redux)/Section 11/01-starting-project/src/components/Cart/Cart.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/Section 11/01-starting-project/src/components/Cart/Cart.js
@@ -1,11 +1,19 @@
+import { useContext } from 'react';
+
 import Modal from '../UI/Modal';
+import CartContext from '../../store/cart-context';
 
 import classes from './Cart.module.css';
 
 const Cart = (props) => {
+	const cartCtx = useContext(CartContext);
+
+	const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+	const hasItems = cartCtx.items.length > 0;
+
 	const cartItems = (
 		<ul className={classes['cart-items']}>
-			{[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
+			{cartCtx.items.map((item) => (
 				<li>{item.name}</li>
 			))}
 		</ul>
@@ -16,7 +24,7 @@ const Cart = (props) => {
 			{cartItems}
 			<div className={classes.total}>
 				<span>Total Amount:</span>
-				<span>35.62</span>
+				<span>{totalAmount}</span>
 			</div>
 			<div className={classes.actions}>
 				<button
@@ -25,7 +33,7 @@ const Cart = (props) => {
 				>
 					Close
 				</button>
-				<button className={classes.button}>Order</button>
+				{hasItems && <button className={classes.button}>Order</button>}
 			</div>
 		</Modal>
 	);
