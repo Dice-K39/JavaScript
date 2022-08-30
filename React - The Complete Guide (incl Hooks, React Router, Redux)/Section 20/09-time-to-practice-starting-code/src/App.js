d50f072a81011fa25bcd76ff9d2c06bcb0ac8013
Dice-K39 <daisukehashimoto39@gmail.com>
2022-08-30 16:35:50 -0400

    Added routing and pages to project.

--- React - The Complete Guide (incl Hooks, React Router, Redux)/Section 20/09-time-to-practice-starting-code/src/App.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/Section 20/09-time-to-practice-starting-code/src/App.js
@@ -1,9 +1,26 @@
-function App() {
-  return (
-    <div>
+import { Route, Switch } from 'react-router-dom';
 
-    </div>
-  );
+import AllQuotes from './pages/AllQuotes';
+import QuoteDetail from './pages/QuoteDetail';
+import NewQuote from './pages/NewQuote';
+
+function App() {
+	return (
+		<Switch>
+			<Route
+				path="/quotes"
+				exact
+			>
+				<AllQuotes />
+			</Route>
+			<Route path="/quotes/:quoteId">
+				<QuoteDetail />
+			</Route>
+			<Route path="/new-quote">
+				<NewQuote />
+			</Route>
+		</Switch>
+	);
 }
 
 export default App;
