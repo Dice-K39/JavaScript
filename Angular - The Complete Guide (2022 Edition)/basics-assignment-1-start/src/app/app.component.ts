f59550bfb9d1a38313bec1ec4b833413cb150029
Dice-K39 <daisukehashimoto39@gmail.com>
2022-09-07 10:04:54 -0400

    Cleared Angular folder to start fresh.

--- Angular - The Complete Guide (2022 Edition)/basics-assignment-1-start/src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.htm',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-}
