34c452d08cb3f519c91c3ef5891cce4009224a11
Dice-K39 <daisukehashimoto39@gmail.com>
2022-09-09 08:07:17 -0400

    Added feedback to user if an error occured.

--- React - The Complete Guide (incl Hooks, React Router, Redux)/Section 22/01-starting-project/src/components/Auth/AuthForm.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/Section 22/01-starting-project/src/components/Auth/AuthForm.js
@@ -6,6 +6,7 @@ const AuthForm = () => {
 	const emailInputRef = useRef();
 	const passwordInputRef = useRef();
 	const [isLogin, setIsLogin] = useState(true);
+	const [isLoading, setIsLoading] = useState(false);
 
 	const switchAuthModeHandler = () => {
 		setIsLogin((prevState) => !prevState);
@@ -16,6 +17,8 @@ const AuthForm = () => {
 
 		const enteredEmail = emailInputRef.current.value;
 		const enteredPassword = passwordInputRef.current.value;
+
+		setIsLoading(true);
 
 		if (isLogin) {
 		} else {
@@ -33,10 +36,18 @@ const AuthForm = () => {
 					}
 				}
 			).then((res) => {
+				setIsLoading(false);
+
 				if (res.ok) {
 				} else {
 					return res.json().then((data) => {
-						console.log(data);
+						let errorMessage = 'Authentication failed!';
+
+						// if (data && data.error && data.error.message) {
+						// 	errorMessage = data.error.message;
+						// }
+
+						alert(errorMessage);
 					});
 				}
 			});
@@ -66,7 +77,8 @@ const AuthForm = () => {
 					/>
 				</div>
 				<div className={classes.actions}>
-					<button>{isLogin ? 'Login' : 'Create Account'}</button>
+					{!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+					{isLoading && <p>Sending request....</p>}
 					<button
 						type="button"
 						className={classes.toggle}
