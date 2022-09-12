050c4abf134a4113395582aea23d080f86a1fca4
Dice-K39 <daisukehashimoto39@gmail.com>
2022-09-12 10:44:38 -0400

    Added code to protect pages from being accessed without authentication.

--- React - The Complete Guide (incl Hooks, React Router, Redux)/Section 22/01-starting-project/src/App.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/Section 22/01-starting-project/src/App.js
@@ -1,11 +1,15 @@
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { useContext } from 'react';
 
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import AuthContext from './store/auth-context';
 
 function App() {
+	const authCtx = useContext(AuthContext);
+
 	return (
 		<Layout>
 			<Switch>
@@ -15,11 +19,17 @@ function App() {
 				>
 					<HomePage />
 				</Route>
-				<Route path="/auth">
-					<AuthPage />
-				</Route>
+				{!authCtx.isLoggedIn && (
+					<Route path="/auth">
+						<AuthPage />
+					</Route>
+				)}
 				<Route path="/profile">
-					<UserProfile />
+					{authCtx.isLoggedIn && <UserProfile />}
+					{!authCtx.isLoggedIn && <Redirect to="/auth" />}
+				</Route>
+				<Route path="*">
+					<Redirect to="/" />
 				</Route>
 			</Switch>
 		</Layout>
