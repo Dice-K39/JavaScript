c0c56f798fe746edd1c4bf03318f89dfaf27ebbd
Dice-K39 <daisukehashimoto39@gmail.com>
2022-09-14 12:24:26 -0400

    Finished assignment on directives.

---
+++ Angular - The Complete Guide (2022 Edition)/basics-assignment-3-start/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+	displayParagraph = false;
+	log = [];
+
+	onDisplay() {
+		this.displayParagraph = !this.displayParagraph;
+		this.log.push(this.log.length + 1);
+	}
+}
