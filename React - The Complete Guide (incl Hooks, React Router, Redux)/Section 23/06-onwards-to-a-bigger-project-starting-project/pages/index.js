3458033976a229fe59fc2ffbe8c59788a0ec0926
Dice-K39 <daisukehashimoto39@gmail.com>
2022-09-16 09:32:47 -0400

    Added useEffect to simulate loading from a server which points out a problem with NextJS.

--- React - The Complete Guide (incl Hooks, React Router, Redux)/Section 23/06-onwards-to-a-bigger-project-starting-project/pages/index.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/Section 23/06-onwards-to-a-bigger-project-starting-project/pages/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import MeetupList from '../components/meetups/MeetupList';
 
 const DUMMY_MEETUPS = [
@@ -5,20 +6,27 @@ const DUMMY_MEETUPS = [
 		id: 'm1',
 		title: 'A First Meetup',
 		image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-		adress: 'Some address 5, 12345 Some City',
+		address: 'Some address 5, 12345 Some City',
 		description: 'This is a first meetup!'
 	},
 	{
 		id: 'm2',
 		title: 'A Second Meetup',
 		image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-		adress: 'Some address 10, 12345 Some City',
+		address: 'Some address 10, 12345 Some City',
 		description: 'This is a second meetup!'
 	}
 ];
 
 function HomePage() {
-	return <MeetupList meetups={DUMMY_MEETUPS} />;
+	const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+	useEffect(() => {
+		// send a http request and fetch data
+		setLoadedMeetups(DUMMY_MEETUPS);
+	}, []);
+
+	return <MeetupList meetups={loadedMeetups} />;
 }
 
 export default HomePage;
