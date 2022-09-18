e407175a99b834b8e99c99cff9d788f7ff51d596
Dice-K39 <daisukehashimoto39@gmail.com>
2022-09-18 09:22:08 -0400

    Added useRouter to programmatically redirect user to homepage.

--- React - The Complete Guide (incl Hooks, React Router, Redux)/Section 23/06-onwards-to-a-bigger-project-starting-project/pages/new-meetup/index.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/Section 23/06-onwards-to-a-bigger-project-starting-project/pages/new-meetup/index.js
@@ -1,8 +1,12 @@
 // our-domain.com/new-meetup
 
+import { useRouter } from 'next/router';
+
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
+	const router = useRouter();
+
 	async function addMeetupHandler(enteredMeetupData) {
 		const response = await fetch('/api/new-meetup', {
 			method: 'POST',
@@ -15,6 +19,8 @@ function NewMeetupPage() {
 		const data = await response.json();
 
 		console.log(data);
+
+		router.push('/');
 	}
 
 	return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
