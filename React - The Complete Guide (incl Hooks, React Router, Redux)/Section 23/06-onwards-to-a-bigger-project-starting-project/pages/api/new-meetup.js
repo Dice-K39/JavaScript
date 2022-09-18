da16d0fbb59ce43b9c7212157cc9200df3620ff4
Dice-K39 <daisukehashimoto39@gmail.com>
2022-09-18 09:09:14 -0400

    Created a connection with MongoDB to insert data into database.

--- React - The Complete Guide (incl Hooks, React Router, Redux)/Section 23/06-onwards-to-a-bigger-project-starting-project/pages/api/new-meetup.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/Section 23/06-onwards-to-a-bigger-project-starting-project/pages/api/new-meetup.js
@@ -1,10 +1,22 @@
+import { MongoClient } from 'mongodb';
+
 // /api/new-meetup
 // POST /api/new-meetup
 
-function handler(req, res) {
+async function handler(req, res) {
 	if (req.method === 'POST') {
 		const data = req.body;
-		const { title, image, address, description } = data;
+
+		const client = await MongoClient.connect(process.env.REACT_APP_MONGODB_CONNECTION_STRING);
+		const db = client.db();
+		const meetupsCollection = db.collection('meetups');
+		const result = await meetupsCollection.insertOne(data);
+
+		console.log(result);
+
+		client.close();
+
+		res.status(201).json({ message: 'Meetup inserted!' });
 	}
 }
 
