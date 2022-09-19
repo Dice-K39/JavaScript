48b52a42e2eda17a91dee13d3e1da0757c393002
Dice-K39 <daisukehashimoto39@gmail.com>
2022-09-19 15:46:33 -0400

    Added code to get data from MongoDB database when page loads.

--- React - The Complete Guide (incl Hooks, React Router, Redux)/Section 23/06-onwards-to-a-bigger-project-starting-project/pages/index.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/Section 23/06-onwards-to-a-bigger-project-starting-project/pages/index.js
@@ -1,21 +1,6 @@
-import MeetupList from '../components/meetups/MeetupList';
+import { MongoClient } from 'mongodb';
 
-const DUMMY_MEETUPS = [
-	{
-		id: 'm1',
-		title: 'A First Meetup',
-		image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-		address: 'Some address 5, 12345 Some City',
-		description: 'This is a first meetup!'
-	},
-	{
-		id: 'm2',
-		title: 'A Second Meetup',
-		image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-		address: 'Some address 10, 12345 Some City',
-		description: 'This is a second meetup!'
-	}
-];
+import MeetupList from '../components/meetups/MeetupList';
 
 function HomePage(props) {
 	return <MeetupList meetups={props.meetups} />;
@@ -38,9 +23,22 @@ function HomePage(props) {
 // reserved function name, will not be visible to client side
 export async function getStaticProps() {
 	// fetch data from an API, database, etc.
+
+	const client = await MongoClient.connect(process.env.REACT_APP_MONGODB_CONNECTION_STRING);
+	const db = client.db();
+	const meetupsCollection = db.collection('meetups');
+	const meetups = await meetupsCollection.find().toArray();
+
+	client.close();
+
 	return {
 		props: {
-			meetups: DUMMY_MEETUPS
+			meetups: meetups.map((meetup) => ({
+				title: meetup.title,
+				address: meetup.address,
+				image: meetup.image,
+				id: meetup._id.toString()
+			}))
 		},
 		// incremental static generation (number of seconds): reevaluates the site and updates it
 		revalidate: 1
