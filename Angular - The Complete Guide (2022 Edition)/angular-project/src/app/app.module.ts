e08f6a35e8d463505f2155c45ca176c37c97f8c3
Dice-K39 <daisukehashimoto39@gmail.com>
2022-09-25 16:28:46 -0400

    Used the components to display sample messages on browser.

--- Angular - The Complete Guide (2022 Edition)/angular-project/src/app/app.module.ts
+++ Angular - The Complete Guide (2022 Edition)/angular-project/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
@@ -15,7 +15,7 @@ import { RecipesComponent } from './recipes/recipes.component';
 		AppComponent,
 		HeaderComponent,
 		ShoppingListComponent,
-		ShoppingListEditComponent,
+		ShoppingEditComponent,
 		RecipeListComponent,
 		RecipeItemComponent,
 		RecipeDetailComponent,
