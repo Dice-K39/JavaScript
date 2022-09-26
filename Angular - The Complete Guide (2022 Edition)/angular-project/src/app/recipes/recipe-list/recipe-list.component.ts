2709a027a189a48cfb94b08fbcfb6fec0ae26f2e
Dice-K39 <daisukehashimoto39@gmail.com>
2022-09-26 10:40:22 -0400

    Used ngFor to output a list of recipes.

--- Angular - The Complete Guide (2022 Edition)/angular-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular - The Complete Guide (2022 Edition)/angular-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
 @Component({
 	selector: 'app-recipe-list',
 	templateUrl: './recipe-list.component.html',
 	styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-	recipes = [];
+	recipes: Recipe[] = [
+		new Recipe(
+			'A Test Recipe',
+			'This is simply a test',
+			'https://www.thereciperebel.com/wp-content/uploads/2022/05/chicken-spaghetti-TRR-1200-24-of-49-568x498.jpg'
+		)
+	];
 
 	constructor() {}
 
