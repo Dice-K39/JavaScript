4597c0783147d4fcfdbacb35ab32d0dc92e771fb
Dice-K39 <daisukehashimoto39@gmail.com>
2022-10-07 09:47:06 -0400

    Used HostBinding to bind to host properties.

--- Angular - The Complete Guide (2022 Edition)/directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ Angular - The Complete Guide (2022 Edition)/directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -1,9 +1,12 @@
-import { Directive, OnInit, ElementRef, Renderer2, HostListener } from '@angular/core';
+import { Directive, OnInit, ElementRef, Renderer2, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
 	selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+	@HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+	@HostBinding('style.color') textColor: string = 'black';
+
 	constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 
 	ngOnInit() {
@@ -11,16 +14,24 @@ export class BetterHighlightDirective implements OnInit {
 	}
 
 	@HostListener('mouseenter') mouseover(eventData: Event) {
-		this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
-		this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
+		// this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+		// this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
+
+		this.backgroundColor = 'blue';
+		this.textColor = 'white';
 	}
 
 	@HostListener('mouseleave') mouseleave(eventData: Event) {
-		this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
-		this.renderer.setStyle(this.elRef.nativeElement, 'color', 'black');
+		// this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+		// this.renderer.setStyle(this.elRef.nativeElement, 'color', 'black');
+
+		this.backgroundColor = 'transparent';
+		this.textColor = 'black';
 	}
 
 	@HostListener('click') click(eventData: Event) {
-		this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'red');
+		// this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'red');
+
+		this.backgroundColor = 'red';
 	}
 }
