aaeec8e1d6591764f30fe795ba991b10a2808c7f
Dice-K39 <daisukehashimoto39@gmail.com>
2022-10-08 15:04:12 -0400

    Added code that closes dropdown menu when clicked anywhere on document.

--- Angular - The Complete Guide (2022 Edition)/angular-project/src/app/shared/dropdown.directive.ts
+++ Angular - The Complete Guide (2022 Edition)/angular-project/src/app/shared/dropdown.directive.ts
@@ -5,9 +5,15 @@ import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from '@an
 })
 export class DropdownDirective {
 	@HostBinding('class.open') isOpen = false;
-	constructor() {}
+	constructor(private elRef: ElementRef) {}
 
-	@HostListener('click') toggleOpen() {
-		this.isOpen = !this.isOpen;
+	// toggling dropdown when clicked on the dropdown menu itself
+	// @HostListener('click') toggleOpen() {
+	// 	this.isOpen = !this.isOpen;
+	// }
+
+	// closes dropdown menu when clicked anywhere on the document
+	@HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+		this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
 	}
 }
