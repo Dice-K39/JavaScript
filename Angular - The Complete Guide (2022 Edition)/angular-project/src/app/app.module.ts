c05a35942b7439afbfb45925fe2a0576cf5da722
Dice-K39 <daisukehashimoto39@gmail.com>
2022-10-24 13:32:29 -0400

    Added child routes.

--- Angular - The Complete Guide (2022 Edition)/angular-project/src/app/app.module.ts
+++ Angular - The Complete Guide (2022 Edition)/angular-project/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 
 @NgModule({
 	declarations: [
@@ -24,7 +25,8 @@ import { AppRoutingModule } from './app-routing.module';
 		RecipeItemComponent,
 		RecipeDetailComponent,
 		RecipesComponent,
-		DropdownDirective
+		DropdownDirective,
+  RecipeStartComponent
 	],
 	imports: [BrowserModule, FormsModule, AppRoutingModule],
 	providers: [ShoppingListService],
