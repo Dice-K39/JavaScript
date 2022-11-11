bab13c75724493bef9b7ee0b35f9cdb9afbf19d3
Dice-K39 <daisukehashimoto39@gmail.com>
2022-11-11 08:09:08 -0500

    Added code to form.

--- Angular - The Complete Guide (2022 Edition)/forms-td-assignment-start/src/app/app.component.ts
+++ Angular - The Complete Guide (2022 Edition)/forms-td-assignment-start/src/app/app.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+	@ViewChild('f', { static: false }) signupForm: NgForm;
+	user = {
+		email: '',
+		subscription: '',
+		password: ''
+	};
+
+	onSubmit() {}
+}
