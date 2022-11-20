839966336442910118473b4743034158c354adca
Dice-K39 <daisukehashimoto39@gmail.com>
2022-11-20 12:25:48 -0500

    Finished assignment.

--- Angular - The Complete Guide (2022 Edition)/forms-reactive-assignment-start/src/app/app.component.ts
+++ Angular - The Complete Guide (2022 Edition)/forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,8 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+	projectForm: FormGroup;
+	forbiddenProjectNames = ['Test'];
+
+	ngOnInit() {
+		this.projectForm = new FormGroup({
+			projectData: new FormGroup({
+				projectName: new FormControl(
+					null,
+					[Validators.required, this.forbiddenNames],
+					this.asyncForbiddenNames
+				),
+				email: new FormControl(null, [Validators.required, Validators.email]),
+				status: new FormControl('')
+			})
+		});
+	}
+
+	onSubmit() {
+		console.log(this.projectForm);
+	}
+
+	forbiddenNames(control: FormControl): { [s: string]: boolean } {
+		if (control.value === 'Test') {
+			return { nameIsForbidden: true };
+		}
+
+		return null;
+	}
+
+	asyncForbiddenNames(control: FormControl): Promise<any> | Observable<any> {
+		const promise = new Promise<any>((resolve, reject) => {
+			setTimeout(() => {
+				if (control.value === 'TestProject') {
+					resolve({ nameIsForbidden: true });
+				} else {
+					resolve(null);
+				}
+			}, 1500);
+		});
+
+		return promise;
+	}
+}
