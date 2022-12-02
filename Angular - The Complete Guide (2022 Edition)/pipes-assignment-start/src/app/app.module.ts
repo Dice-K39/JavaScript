7a375dc995b20b6752a39656c61441a20a90163e
Dice-K39 <daisukehashimoto39@gmail.com>
2022-12-02 08:04:58 -0500

    Created a reverse pipe.

--- Angular - The Complete Guide (2022 Edition)/pipes-assignment-start/src/app/app.module.ts
+++ Angular - The Complete Guide (2022 Edition)/pipes-assignment-start/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ReversePipe } from './reverse.pipe';
 
 @NgModule({
-	declarations: [AppComponent],
+	declarations: [AppComponent, ReversePipe],
 	imports: [BrowserModule, FormsModule],
 	providers: [],
 	bootstrap: [AppComponent]
