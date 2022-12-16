b9fcc17dba953e7d145cc9cc7b5acae208943b08
Dice-K39 <daisukehashimoto39@gmail.com>
2022-12-16 08:07:07 -0500

    Added a resolver to resolve data before loading.

--- Angular - The Complete Guide (2022 Edition)/angular-project/src/app/shared/data-storage.service.ts
+++ Angular - The Complete Guide (2022 Edition)/angular-project/src/app/shared/data-storage.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 import { RecipeService } from '../recipes/recipe.service';
 import { environment } from '../../my-environment/environment';
@@ -12,27 +12,25 @@ export class DataStorageService {
 
 	storeRecipes() {
 		const recipes = this.recipeService.getRecipes();
-
+		console.log(environment.API_URL);
 		this.http.put(`${environment.API_URL}/recipes.json`, recipes).subscribe((response) => {
 			console.log(response);
 		});
 	}
 
 	fetchRecipes() {
-		return this.http
-			.get<Recipe[]>(`${environment.API_URL}/recipes.json`)
-			.pipe(
-				map((recipes) => {
-					return recipes.map((recipe) => {
-						return {
-							...recipe,
-							ingredients: recipe.ingredients ? recipe.ingredients : []
-						};
-					});
-				})
-			)
-			.subscribe((recipes) => {
+		return this.http.get<Recipe[]>(`${environment.API_URL}/recipes.json`).pipe(
+			map((recipes) => {
+				return recipes.map((recipe) => {
+					return {
+						...recipe,
+						ingredients: recipe.ingredients ? recipe.ingredients : []
+					};
+				});
+			}),
+			tap((recipes) => {
 				this.recipeService.setRecipes(recipes);
-			});
+			})
+		);
 	}
 }
