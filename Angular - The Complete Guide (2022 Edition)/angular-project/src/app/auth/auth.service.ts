0afb236d4598584dfb33603a0c028b02d255f5ec
Dice-K39 <daisukehashimoto39@gmail.com>
2022-12-22 07:51:39 -0500

    Improved error handling by putting some code in the service.

--- Angular - The Complete Guide (2022 Edition)/angular-project/src/app/auth/auth.service.ts
+++ Angular - The Complete Guide (2022 Edition)/angular-project/src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 import { environment } from '../../my-environment/environment';
 
@@ -16,13 +18,30 @@ interface AuthResponseData {
 export class AuthService {
 	constructor(private http: HttpClient) {}
 	signup(email: string, password: string) {
-		return this.http.post<AuthResponseData>(
-			`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${environment.API_KEY}`,
-			{
-				email,
-				password,
-				returnSecureToken: true
-			}
-		);
+		return this.http
+			.post<AuthResponseData>(
+				`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${environment.API_KEY}`,
+				{
+					email,
+					password,
+					returnSecureToken: true
+				}
+			)
+			.pipe(
+				catchError((errorRes) => {
+					let errorMessage = 'An unknown error occurred!"';
+
+					if (!errorRes.error || !errorRes.error.error) {
+						return throwError(errorMessage);
+					}
+
+					switch (errorRes.error.error.message) {
+						case 'EMAIL_EXISTS':
+							errorMessage = 'This email exists already!';
+					}
+
+					return throwError(errorMessage);
+				})
+			);
 	}
 }
