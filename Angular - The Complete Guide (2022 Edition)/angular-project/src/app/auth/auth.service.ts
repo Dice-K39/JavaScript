ea575d12140cb54a5b14948981a8adc39725b819
Dice-K39 <daisukehashimoto39@gmail.com>
2022-12-25 09:25:21 -0500

    Added code that will change the header if user is logged in or not.

--- Angular - The Complete Guide (2022 Edition)/angular-project/src/app/auth/auth.service.ts
+++ Angular - The Complete Guide (2022 Edition)/angular-project/src/app/auth/auth.service.ts
@@ -19,6 +19,7 @@ export interface AuthResponseData {
 @Injectable({ providedIn: 'root' })
 export class AuthService {
 	user = new Subject<User>();
+
 	constructor(private http: HttpClient) {}
 
 	signup(email: string, password: string) {
