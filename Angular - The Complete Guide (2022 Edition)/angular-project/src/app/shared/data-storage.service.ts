75c7aef8c14086c8ccd6d4923cd748cdfd3017f4
Dice-K39 <daisukehashimoto39@gmail.com>
2022-12-26 09:15:09 -0500

    Added code that adds the token to outgoing requests.

--- Angular - The Complete Guide (2022 Edition)/angular-project/src/app/shared/data-storage.service.ts
+++ Angular - The Complete Guide (2022 Edition)/angular-project/src/app/shared/data-storage.service.ts
@@ -1,14 +1,19 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, tap } from 'rxjs/operators';
+import { exhaustMap, map, take, tap } from 'rxjs/operators';
 
 import { RecipeService } from '../recipes/recipe.service';
 import { environment } from '../../my-environment/environment';
 import { Recipe } from '../recipes/recipe.model';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
-	constructor(private http: HttpClient, private recipeService: RecipeService) {}
+	constructor(
+		private http: HttpClient,
+		private recipeService: RecipeService,
+		private authService: AuthService
+	) {}
 
 	storeRecipes() {
 		const recipes = this.recipeService.getRecipes();
@@ -19,7 +24,13 @@ export class DataStorageService {
 	}
 
 	fetchRecipes() {
-		return this.http.get<Recipe[]>(`${environment.API_URL}/recipes.json`).pipe(
+		return this.authService.user.pipe(
+			take(1),
+			exhaustMap((user) => {
+				return this.http.get<Recipe[]>(`${environment.API_URL}/recipes.json`, {
+					params: new HttpParams().set('auth', user.token)
+				});
+			}),
 			map((recipes) => {
 				return recipes.map((recipe) => {
 					return {
