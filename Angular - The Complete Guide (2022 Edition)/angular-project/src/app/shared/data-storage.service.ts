11200ad94a735af4dbf1401f39aae88efa2f3fa0
Dice-K39 <daisukehashimoto39@gmail.com>
2022-12-27 08:04:03 -0500

    Added an interceptor to attach a token to outgoing requests.

--- Angular - The Complete Guide (2022 Edition)/angular-project/src/app/shared/data-storage.service.ts
+++ Angular - The Complete Guide (2022 Edition)/angular-project/src/app/shared/data-storage.service.ts
@@ -24,13 +24,7 @@ export class DataStorageService {
 	}
 
 	fetchRecipes() {
-		return this.authService.user.pipe(
-			take(1),
-			exhaustMap((user) => {
-				return this.http.get<Recipe[]>(`${environment.API_URL}/recipes.json`, {
-					params: new HttpParams().set('auth', user.token)
-				});
-			}),
+		return this.http.get<Recipe[]>(`${environment.API_URL}/recipes.json`).pipe(
 			map((recipes) => {
 				return recipes.map((recipe) => {
 					return {
