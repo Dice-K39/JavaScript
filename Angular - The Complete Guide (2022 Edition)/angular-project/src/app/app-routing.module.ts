1256b22f2581167c69598f266cabc2957f07860e
Dice-K39 <daisukehashimoto39@gmail.com>
2023-01-08 08:41:14 -0500

    Modified code to include lazy loading.

--- Angular - The Complete Guide (2022 Edition)/angular-project/src/app/app-routing.module.ts
+++ Angular - The Complete Guide (2022 Edition)/angular-project/src/app/app-routing.module.ts
@@ -3,7 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AuthComponent } from './auth/auth.component';
 
-const appRoutes: Routes = [{ path: '', redirectTo: '/recipes', pathMatch: 'full' }];
+const appRoutes: Routes = [
+	{ path: '', redirectTo: '/recipes', pathMatch: 'full' },
+	{
+		path: 'recipes',
+		loadChildren: () => import('./recipes/recipes.module').then((m) => m.RecipesModule)
+	}
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(appRoutes)],
