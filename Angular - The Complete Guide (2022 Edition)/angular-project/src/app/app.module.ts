5c1d36d5fdfcec203b9e74697ca6c9e51af5bd58
Dice-K39 <daisukehashimoto39@gmail.com>
2023-01-17 08:14:32 -0500

    Reinstall ngrx/store and setup.

--- Angular - The Complete Guide (2022 Edition)/angular-project/src/app/app.module.ts
+++ Angular - The Complete Guide (2022 Edition)/angular-project/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -8,10 +9,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
 import { LoggingService } from './logging.services';
+import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer';
 
 @NgModule({
 	declarations: [AppComponent, HeaderComponent],
-	imports: [BrowserModule, HttpClientModule, AppRoutingModule, SharedModule, CoreModule],
+	imports: [
+		BrowserModule,
+		HttpClientModule,
+		AppRoutingModule,
+		StoreModule.forRoot({ shoppingList: shoppingListReducer }),
+		SharedModule,
+		CoreModule
+	],
 	bootstrap: [AppComponent],
 	providers: [LoggingService]
 })
