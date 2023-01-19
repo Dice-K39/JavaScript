86d293b448392df1315fc718f2036890b92a16fc
Dice-K39 <daisukehashimoto39@gmail.com>
2023-01-19 07:58:27 -0500

    Added another action to dispatch.

--- Angular - The Complete Guide (2022 Edition)/angular-project/src/app/recipes/recipe.service.ts
+++ Angular - The Complete Guide (2022 Edition)/angular-project/src/app/recipes/recipe.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { Subject } from 'rxjs';
 
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Recipe } from './recipe.model';
+import * as ShoppingListActions from '../shopping-list/store/shopping-list.actions';
 
 @Injectable()
 export class RecipeService {
@@ -25,7 +27,10 @@ export class RecipeService {
 	// 	)
 	// ];
 
-	constructor(private shoppingListService: ShoppingListService) {}
+	constructor(
+		private shoppingListService: ShoppingListService,
+		private store: Store<{ shoppingList: { ingredients: Ingredient[] } }>
+	) {}
 
 	setRecipes(recipes: Recipe[]) {
 		this.recipes = recipes;
@@ -41,7 +46,8 @@ export class RecipeService {
 	}
 
 	addIngredientsToShoppingList(ingredients: Ingredient[]) {
-		this.shoppingListService.addIngredients(ingredients);
+		// this.shoppingListService.addIngredients(ingredients);
+		this.store.dispatch(new ShoppingListActions.AddIngredients(ingredients));
 	}
 
 	addRecipe(recipe: Recipe) {
