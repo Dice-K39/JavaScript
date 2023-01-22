1c9423d5bf904049cb8f002681e723e7a5ca112e
Dice-K39 <daisukehashimoto39@gmail.com>
2023-01-22 08:39:57 -0500

    Added more actions to be managed using NgRx.

--- Angular - The Complete Guide (2022 Edition)/angular-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ Angular - The Complete Guide (2022 Edition)/angular-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -26,14 +26,17 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
 	) {}
 
 	ngOnInit(): void {
-		this.subscription = this.shoppingListService.startedEditing.subscribe((index: number) => {
-			this.editedItemIndex = index;
-			this.editMode = true;
-			this.editedItem = this.shoppingListService.getIngredient(index);
-			this.shoppingListForm.setValue({
-				name: this.editedItem.name,
-				amount: this.editedItem.amount
-			});
+		this.subscription = this.store.select('shoppingList').subscribe((stateData) => {
+			if (stateData.editedIngredientIndex > -1) {
+				this.editMode = true;
+				this.editedItem = stateData.editedIngredient;
+				this.shoppingListForm.setValue({
+					name: this.editedItem.name,
+					amount: this.editedItem.amount
+				});
+			} else {
+				this.editMode = false;
+			}
 		});
 	}
 
@@ -61,6 +64,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
 	onClear() {
 		this.shoppingListForm.reset();
 		this.editMode = false;
+		this.store.dispatch(new ShoppingListActions.StopEdit());
 	}
 
 	onDelete() {
@@ -71,5 +75,6 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
 
 	ngOnDestroy() {
 		this.subscription.unsubscribe();
+		this.store.dispatch(new ShoppingListActions.StopEdit());
 	}
 }
