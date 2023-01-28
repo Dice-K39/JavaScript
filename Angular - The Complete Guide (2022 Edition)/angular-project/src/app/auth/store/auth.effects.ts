84c60c714da7ca31fd859682386bee7e136ea9a6
Dice-K39 <daisukehashimoto39@gmail.com>
2023-01-28 08:54:08 -0500

    Added code in effects to handle error handling.

--- Angular - The Complete Guide (2022 Edition)/angular-project/src/app/auth/store/auth.effects.ts
+++ Angular - The Complete Guide (2022 Edition)/angular-project/src/app/auth/store/auth.effects.ts
@@ -1,9 +1,45 @@
-import { Actions, ofType } from '@ngrx/effects';
+import { HttpClient } from '@angular/common/http';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { catchError, map, switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 import * as AuthActions from './auth.actions';
+import { environment } from '../../../my-environment/environment';
+
+export interface AuthResponseData {
+	kind: string;
+	idToken: string;
+	email: string;
+	refreshToken: string;
+	expiresIn: string;
+	localId: string;
+	registered?: boolean;
+}
 
 export class AuthEffects {
-	authLogin = this.actions$.pipe(ofType(AuthActions.LOGIN_START));
+	@createEffect()
+	authLogin = this.actions$.pipe(
+		ofType(AuthActions.LOGIN_START),
+		switchMap((authData: AuthActions.LoginStart) => {
+			return this.http
+				.post<AuthResponseData>(
+					`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.API_KEY}`,
+					{
+						email: authData.payload.email,
+						password: authData.payload.password,
+						returnSecureToken: true
+					}
+				)
+				.pipe(
+					catchError((error) => {
+						of();
+					}),
+					map((resData) => {
+						of();
+					})
+				);
+		})
+	);
 
-	constructor(private actions$: Actions) {}
+	constructor(private actions$: Actions, private http: HttpClient) {}
 }
