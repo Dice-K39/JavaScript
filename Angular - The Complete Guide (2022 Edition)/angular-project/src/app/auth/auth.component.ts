bc5034a623f97940b6e24ce4626caeea005fbc1f
Dice-K39 <daisukehashimoto39@gmail.com>
2023-02-05 09:45:00 -0500

    Finished signup effect.

--- Angular - The Complete Guide (2022 Edition)/angular-project/src/app/auth/auth.component.ts
+++ Angular - The Complete Guide (2022 Edition)/angular-project/src/app/auth/auth.component.ts
@@ -1,10 +1,10 @@
 import { Component, ComponentFactoryResolver, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Observable, Subscription } from 'rxjs';
+import { Subscription } from 'rxjs';
 import { Store } from '@ngrx/store';
 
-import { AuthResponseData, AuthService } from './auth.service';
+import { AuthService } from './auth.service';
 import { AlertComponent } from '../shared/alert/alert.component';
 import { PlaceholderDirective } from '../shared/placeholder/placeholder.directive';
 import * as fromApp from '../store/app.reducer';
@@ -51,30 +51,11 @@ export class AuthComponent implements OnInit, OnDestroy {
 		const email = form.value.email;
 		const password = form.value.password;
 
-		let authObs: Observable<AuthResponseData>;
-
-		this.isLoading = true;
-
 		if (this.isLoginMode) {
-			// authObs = this.authService.login(email, password);
 			this.store.dispatch(new AuthActions.LoginStart({ email, password }));
 		} else {
-			authObs = this.authService.signup(email, password);
+			this.store.dispatch(new AuthActions.SignupStart({ email, password }));
 		}
-
-		// authObs.subscribe(
-		// 	(resData) => {
-		// 		console.log(resData);
-		// 		this.isLoading = false;
-		// 		this.router.navigate(['/recipes']);
-		// 	},
-		// 	(errorMessage) => {
-		// 		console.log(errorMessage);
-		// 		this.error = errorMessage;
-		// 		this.showErrorAlert(errorMessage);
-		// 		this.isLoading = false;
-		// 	}
-		// );
 
 		form.reset();
 	}
