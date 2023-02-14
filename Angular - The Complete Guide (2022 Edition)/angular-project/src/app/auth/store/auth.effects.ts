87a7b9e568daa9646c1b0d4c981af4dd056fa5c5
Dice-K39 <daisukehashimoto39@gmail.com>
2023-02-14 07:47:11 -0500

    Fixed redirect bug where a reload would send a user back to the home page.

--- Angular - The Complete Guide (2022 Edition)/angular-project/src/app/auth/store/auth.effects.ts
+++ Angular - The Complete Guide (2022 Edition)/angular-project/src/app/auth/store/auth.effects.ts
@@ -30,7 +30,8 @@ const handleAuthentication = (expiresIn: number, email: string, userId: string, 
 		email,
 		userId,
 		token,
-		expirationDate
+		expirationDate,
+		redirect: true
 	});
 };
 
@@ -125,8 +126,10 @@ export class AuthEffects {
 	@Effect({ dispatch: false })
 	authRedirect = this.actions$.pipe(
 		ofType(AuthActions.AUTHENTICATE_SUCCESS),
-		tap(() => {
-			this.router.navigate(['/']);
+		tap((authSuccessAction: AuthActions.AuthenticateSuccess) => {
+			if (authSuccessAction.payload.redirect) {
+				this.router.navigate(['/']);
+			}
 		})
 	);
 
@@ -163,7 +166,8 @@ export class AuthEffects {
 					email: loadedUser.email,
 					userId: loadedUser.id,
 					token: loadedUser.token,
-					expirationDate: new Date(userData._tokenExpirationDate)
+					expirationDate: new Date(userData._tokenExpirationDate),
+					redirect: false
 				});
 
 				// const expirationDuration =
