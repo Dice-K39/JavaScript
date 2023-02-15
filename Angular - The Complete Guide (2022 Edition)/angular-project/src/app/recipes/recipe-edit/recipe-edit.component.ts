cb8ebe09c80d17229f0208adfe2909508d173c3f
Dice-K39 <daisukehashimoto39@gmail.com>
2023-02-15 08:01:28 -0500

    Added code to dispatch actions.

--- Angular - The Complete Guide (2022 Edition)/angular-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ Angular - The Complete Guide (2022 Edition)/angular-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -6,6 +6,7 @@ import { map } from 'rxjs/operators';
 
 import { RecipeService } from '../recipe.service';
 import * as fromApp from '../../store/app.reducer';
+import * as RecipesActions from '../store/recipe.actions';
 
 @Component({
 	selector: 'app-recipe-edit',
@@ -41,9 +42,16 @@ export class RecipeEditComponent implements OnInit {
 		// );
 
 		if (this.editMode) {
-			this.recipeService.updateRecipe(this.id, this.recipeForm.value);
+			// this.recipeService.updateRecipe(this.id, this.recipeForm.value);
+			this.store.dispatch(
+				new RecipesActions.UpdateRecipe({
+					index: this.id,
+					newRecipe: this.recipeForm.value
+				})
+			);
 		} else {
-			this.recipeService.addRecipe(this.recipeForm.value);
+			// this.recipeService.addRecipe(this.recipeForm.value);
+			this.store.dispatch(new RecipesActions.AddRecipe(this.recipeForm.value));
 		}
 
 		this.onCancel();
