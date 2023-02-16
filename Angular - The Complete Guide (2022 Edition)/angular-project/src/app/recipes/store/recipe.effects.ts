8697cc5cdeb7fbfbc351781ba1f674545b7c43b5
Dice-K39 <daisukehashimoto39@gmail.com>
2023-02-16 08:00:04 -0500

    Added effect to store recipes.

--- Angular - The Complete Guide (2022 Edition)/angular-project/src/app/recipes/store/recipe.effects.ts
+++ Angular - The Complete Guide (2022 Edition)/angular-project/src/app/recipes/store/recipe.effects.ts
@@ -1,11 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Actions, Effect, ofType } from '@ngrx/effects';
-import { switchMap, map } from 'rxjs/operators';
+import { switchMap, map, withLatestFrom } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
 
 import * as RecipesActions from './recipe.actions';
 import { Recipe } from '../recipe.model';
 import { environment } from '../../../my-environment/environment';
+import * as fromApp from '../../store/app.reducer';
 
 @Injectable()
 export class RecipeEffects {
@@ -28,5 +30,18 @@ export class RecipeEffects {
 		})
 	);
 
-	constructor(private actions$: Actions, private http: HttpClient) {}
+	@Effect({ dispatch: false })
+	storeRecipes = this.actions$.pipe(
+		ofType(RecipesActions.STORE_RECIPES),
+		withLatestFrom(this.store.select('recipes')),
+		switchMap(([actionData, recipesState]) => {
+			return this.http.put(`${environment.API_URL}/recipes.json`, recipesState.recipes);
+		})
+	);
+
+	constructor(
+		private actions$: Actions,
+		private http: HttpClient,
+		private store: Store<fromApp.AppState>
+	) {}
 }
